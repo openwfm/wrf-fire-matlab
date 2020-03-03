7b1d35b4b5b87ce13eac3349318611f0c05d9ac0
Angel Farguell Caus <angel.farguell@gmail.com>
2020-03-02 22:49:08 -0700

    fixing creation of B

--- quicwind/saddlepoint/cell_B.m
+++ quicwind/saddlepoint/cell_B.m
@@ -23,16 +23,8 @@ txk = (z(i+1,j,k)-z(i,j,k)+z(i+1,j+1,k)-z(i,j+1,k))/(2*dx); % tangent of thetax 
 tyk = (z(i,j+1,k)-z(i,j,k)+z(i+1,j+1,k)-z(i+1,j,k))/(2*dy); % tangent of thetay at k
 txk1 = (z(i+1,j,k+1)-z(i,j,k+1)+z(i+1,j+1,k+1)-z(i,j+1,k+1))/(2*dx); % tangent of thetax at k+1
 tyk1 = (z(i,j+1,k+1)-z(i,j,k+1)+z(i+1,j+1,k+1)-z(i+1,j,k+1))/(2*dy); % tangent of thetay at k+1
-thetax = atan(txk), thetay = atan(tyk)
-% sines
-sxk = txk/sqrt(1+txk*txk);
-syk = tyk/sqrt(1+tyk*tyk);
-err = sxk-sin(thetax), err = syk-sin(thetay)
-sxk1 = txk1/sqrt(1+txk1*txk1);
-syk1 = tyk1/sqrt(1+tyk1*tyk1);
 % secants 
 sck = sqrt((1+txk*txk)*(1+tyk*tyk)); % sec(thetax)*sec(thetay) at k 
-err = sck-sec(thetax)*sec(thetay)
 sck1 = sqrt((1+txk1*txk1)*(1+tyk1*tyk1)); % sec(thetax)*sec(thetay) at k+1
 % areas at the faces
 a1 = dy*.5*(z(i,j,k+1)-z(i,j,k)+z(i,j+1,k+1)-z(i,j+1,k)); % left face area
@@ -42,34 +34,14 @@ a4 = dx*.5*(z(i,j+1,k+1)-z(i,j+1,k)+z(i+1,j+1,k+1)-z(i+1,j+1,k)); % front face a
 a5 = (dx*dy)*sck; % bottom face area 
 a6 = (dx*dy)*sck1; % top face area
 a = [a1,a2,a3,a4,a5,a6];
-B1 = diag(1./a); % scale flux to wind vectors into the normal direction on sides 
-% u = v./a;
-% decompose the bottom flux into cartesian components
-% the bottom flux is in cartesian components: 
-%       ux = sin(thetax)*u
-%       uy = sin(thetay)*u
-%       uz = -sqrt(1-sin(thetax)^2-sin(thetay)^2)*u
-% B2*u = wind through 
-%   (leftx,rightx,fronty,backy,bottomx,bottomy,bottomz,topx,topy,topz)
-B2 = [-1,0,0,0,0,0; % 1: wind in x direction caused by left flux
-      0,1,0,0,0,0; % 2: wind in x direction caused by right flux
-      0,0,-1,0,0,0; % 3: wind in y direction caused by front flux
-      0,0,0,1,0,0; % 4: wind in y direction caused by back flux
-      0,0,0,0,sxk,0; % 5: wind in x direction caused by bottom flux 
-      0,0,0,0,syk,0; % 6: wind in y direction caused by bottom flux 
-      0,0,0,0,-sqrt(1-sxk*sxk-syk*syk),0; % 7: wind in z direction caused by bottom flux 
-      0,0,0,0,0,-sxk1; % 8: wind in x direction caused by top flux 
-      0,0,0,0,0,-syk1; % 9: wind in y direction caused by top flux 
-      0,0,0,0,0,sqrt(1-sxk1*sxk1-syk1*syk1)]; % 10: wind in z direction caused by top flux 
-% combine left, front, and bottom into a single wind vector v1, and 
-% combine right, back, and top into a single wind vector v2
-B3 = [1,0,0,0,1,0,0,0,0,0; % v1(1) 
-      0,1,0,0,0,0,0,1,0,0; % v2(1)
-      0,0,1,0,0,1,0,0,0,0; % v1(2)
-      0,0,0,1,0,0,0,0,1,0; % v2(2)
-      0,0,0,0,0,0,1,0,0,0; % v1(3)
-      0,0,0,0,0,0,0,0,0,1]; % v2(3)
-disp('columns are: left, right, front, back, bottom, and top');
-disp('rows are: v1(1), v2(1), v1(2), v2(2), v1(3), and v2(3)');
-B = B3*B2*B1;
+Binv = diag(a)* ...
+        [-1,0,0,0,0,0;       % 1: flux through the left face from v1
+         0,1,0,0,0,0;        % 2: flux through the right face from v2
+         0,0,-1,0,0,0;       % 3: flux through the front face from v1
+         0,0,0,1,0,0;        % 4: flux through the back face from v2
+         txk,0,tyk,0,-1,0;   % 5: flux through the bottom face from v1
+         0,-txk1,0,-tyk1,0,1]; % 6: flux through the top face from v2
+%disp('columns are: left, right, front, back, bottom, and top');
+%disp('rows are: v1(1), v2(1), v1(2), v2(2), v1(3), and v2(3)');
+B = inv(Binv);
 end
