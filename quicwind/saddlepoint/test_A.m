0d0ec93d54c1496fe0ed71c6a5a0fb743a5d223e
Angel Farguell Caus <angel.farguell@gmail.com>
2020-03-05 09:51:12 -0700

    improving test_A

--- quicwind/saddlepoint/test_A.m
+++ quicwind/saddlepoint/test_A.m
@@ -1,14 +1,15 @@
+function test_A
 % settings
-levels = 5;
+levels = 3;
 fp = [2,2,2];
 
-% initialize wind
-wp = zeros(levels,3);
+% initialize
+wp = cell(levels,3);
+F = cell(levels,3);
 for r=1:levels
     % dimension
-    n = [5,5,5]*2^(r-1);
+    n = [4,4,4]*2^(r-1);
     h = [2,2,2]/(2^r);
-    factor = 6;
     fprintf('linear array of %ix%ix%i cells\n',n(1),n(2),n(3))
     % creating grid
     X = regular_mesh(n,h,1);
@@ -19,7 +20,6 @@ for r=1:levels
     for k = 1:3
         CX{k} = (X{k}(1:end-1,1:end-1,1:end-1)+X{k}(2:end,1:end-1,1:end-1)+X{k}(1:end-1,2:end,1:end-1)+X{k}(1:end-1,1:end-1,2:end)+X{k}(2:end,2:end,1:end-1)+X{k}(2:end,1:end-1,2:end)+X{k}(1:end-1,2:end,2:end)+X{k}(2:end,2:end,2:end))/8;
     end
-    xx = CX{1}; yy = CX{2}; zz = CX{3};
     % assembly sparse matrices
     [A,D,E,B,C,v0] = sparse_assembly(X,h);
     % solve mass-consistent using saddle problem
@@ -27,11 +27,13 @@ for r=1:levels
     % transform resulting fluxes into cartesian winds at the centers
     u=E*B*v;
     % create interpolant elements from wind at the center of the cells
-    Fx = scatteredInterpolant(CX{1}(:),CX{2}(:),CX{3}(:),u(1:3:end));
-    Fy = scatteredInterpolant(CX{1}(:),CX{2}(:),CX{3}(:),u(2:3:end));
-    Fz = scatteredInterpolant(CX{1}(:),CX{2}(:),CX{3}(:),u(3:3:end));
-    wp(r,1) = Fx(fp); 
-    wp(r,2) = Fy(fp); 
-    wp(r,3) = Fz(fp);
+    F{r,1} = scatteredInterpolant(CX{1}(:),CX{2}(:),CX{3}(:),u(1:3:end));
+    F{r,2} = scatteredInterpolant(CX{1}(:),CX{2}(:),CX{3}(:),u(2:3:end));
+    F{r,3} = scatteredInterpolant(CX{1}(:),CX{2}(:),CX{3}(:),u(3:3:end));
+    wp{r,1} = F{r,1}(fp); 
+    wp{r,2} = F{r,2}(fp); 
+    wp{r,3} = F{r,3}(fp);
 end
-save('test_A','wp');
+d.F = F; d.wp = wp;
+save('test_A','-struct','d');
+end
