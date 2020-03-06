8c30233da7babb90d462b9483931432eeec5fe44
Angel Farguell Caus <angel.farguell@gmail.com>
2020-03-06 13:39:06 -0700

    overleaf implementaiton of cell_P

--- quicwind/saddlepoint/cell_P.m
+++ quicwind/saddlepoint/cell_P.m
@@ -1,13 +1,6 @@
 function P=cell_P(A,B,D)
 
-X = B'*A*B;
-L = chol(X,'lower');
-U = L\eye(size(X));
-ib = L'\U;
-X = D*ib*D';
-L = chol(X,'lower');
-U = L\eye(size(X));
-ibd = L'\U;
-P=ib - ib*D'*ibd*D*ib;
+X = inv(B'*A*B);
+P = X - X*D'*((D*X*D')\(D*X));
 
 end
