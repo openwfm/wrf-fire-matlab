f10fa864a506bd5ad2497ee31cdb70f58aa8a617
James Haley <james.haley@ucdenver.edu>
2020-04-21 18:33:35 -0600

    commenting, streamlining function

--- cycling/fuel_adjust.m
+++ cycling/fuel_adjust.m
@@ -9,16 +9,15 @@ load(load_str);
 
 %nned to beef up what's in red, below
 w  = read_wrfout_tign(restart_file);
+%use analysis to et region for changing fuel moisture
 w.tign_g = p.analysis;
 red = subset_domain(w);
 
 %use the wrfout file to find subset of detections
 
 time_bounds(2) = red.max_tign;
-end_date = datestr(time_bounds(2));
-%time_bounds(2) = 7.354637292824074e+05
 time_bounds(1) = red.min_tign;
-%time_bounds(1) = time_bounds(2)-3;
+
 if fire_choice == 1
     fire_name = 'Camp fire';
     save_name = 'camp';
@@ -47,59 +46,30 @@ end
 min_con = 7;
 for i = 1:length(g)
     if i == 1
-        fire_mask = g(i).data >=min_con;
         fm = g(i).fxdata >=min_con;
-        lons = g(i).xlon(fire_mask);
-        lats = g(i).xlat(fire_mask);
     end
-    fire_mask = g(i).data >= min_con;
-    lon_update = g(i).xlon(fire_mask);
-    lat_update = g(i).xlat(fire_mask);
-    fm = fm + (g(i).fxdata >=min_con);
-    %fprintf('%d detections \n',sum(fm(:)));
-    %looking for bad cougar granule
-    %if max(lat_update(:)) > 46.2 | min(lat_update(:)) < 46.05
-    %    fprintf('bad granule : %s \n',g(i).file)
-    %end
-    if sum(fire_mask(:)) > 0
-        lons = [lons(:);lon_update(:)];
-        lats = [lats(:);lat_update(:)];
-    end
-    %scatter(lons,lats);
-    
+    fm = fm + (g(i).fxdata >=min_con); 
 end %for i...
+
+% flatten fm to height 1
 fm = fm > 0;
 
 forecast_area = red.tign <= g(i).time;
-forecast_lons = red.fxlong(forecast_area);
-forecast_lats = red.fxlat(forecast_area);
 
-%find perimter of detections
-% k = boundary(lons,lats);
-% sat_lons = lons(k);
-% sat_lats = lats(k);
-
-% [in,on] = inpolygon(forecast_lons,forecast_lats,sat_lons,sat_lats);
-% inside = logical(in+on);
-
-mult=1.4;
+mult = input_num('Enter fuel moisture multiplier: ',1)
+%mult=1.4;
 too_far = mult*(forecast_area-fm);
 new2far = ones(size(too_far));
 too_far_mask = too_far > 0;
 new2far(too_far_mask) = too_far(too_far_mask);
 
 fast_fuel = red.nfuel_cat(too_far_mask);
+%figure,histogram(fast_fuel(:))
 
-
+%go to w from red
 w_mask = ones(size(w.tign_g));
 w_mask(red.ispan,red.jspan) = new2far;
 
-%need to code some way to figure which restart file to use
-% if cycle == 1
-%     restart = 'wrfinput_d01'
-% else
-%     restart = 'wrfrst_d01_2013-08-13_00:00:00'
-% end
 
 fprintf('%s %s\n','Will write modified time into     ',restart_file)
 rewrite_bak=[restart_file,'.bak'];
@@ -110,7 +80,7 @@ if q,
     end
 end
 
-%%% change fuel moisture %%%
+%%% change fuel moisture in restart file %%%
 new_moist = w.fmc_g;
 new_moist = new_moist.*w_mask;
 ncreplace(restart_file,'FMC_G',new_moist)
