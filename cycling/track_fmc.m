159adcda1e5fe2b3bbe8fb9ed0a8a21a1fc0e7f7
James Haley <james.haley@ucdenver.edu>
2020-05-16 09:05:21 -0600

    fixing indexing in loop

--- cycling/track_fmc.m
+++ cycling/track_fmc.m
@@ -2,7 +2,8 @@ function tracks = track_fmc(wrf)
 
 t=nc2struct(wrf,{'Times'},{});
 ts = char(t.times');
-for i = 1:length(ts)
+[n,m] = size(ts)
+for i = 1:n
     s = nc2struct(wrf,{'FMC_G'},{},i);
     avg_fmc(i) = mean(s.fmc_g(:));
     step_time(i) = datenum(ts(i,:));
