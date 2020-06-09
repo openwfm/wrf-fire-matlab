73ea706e503326e4d8397676ff4dfa26d330c59e
Jan Mandel <jan.mandel@gmail.com>
2020-06-09 02:01:35 -0600

    adding uah/vah Sat Apr 26 19:02:53 2014 -0600)

--- debug/goprof.m
+++ debug/goprof.m
@@ -1,13 +1,17 @@
- % debugging canopy interpolation
- step=100
- file='em_rxcadre2_devel/wrfout_d05_2012-11-11_12:00:00'
+% debugging canopy interpolation
+ step=5
+ file='wrfout_d05_2012-11-11_12:00:00'
  long=-86.73
  lat=30.53
  p=wrfatm2struct(file,step);
- pp=nc2struct(file,{'CAN_TOP','CUF','CVF','FWH','FZ0'},{},step);
+ pp=nc2struct(file,{'CAN_TOP','CUF','CVF','FWH','FZ0','UF','VF','UAH','VAH'},{},step);
  p.can_top=pp.can_top;
  p.cuf=pp.cuf;
  p.cvf=pp.cvf;
  p.fwh=pp.fwh;
  p.fz0=pp.fz0;
- prof(p,long,lat);
+ p.uf=pp.uf;
+ p.vf=pp.vf;
+ p.uah=pp.uah;
+ p.vah=pp.vah;
+prof(p,long,lat)
--- debug/prof.m
+++ debug/prof.m
@@ -1,5 +1,7 @@
 function [h,u,v,names]=prof(p,long,lat)
-names={'layer1','layer2','layer3','layer4','can_top','fwh'};
+long
+lat
+names={'layer1','layer2','layer3','layer4','can_top','uf/vf','uah/vah'};
 m=4;
 for i=1:m
     h(i)=interps(p.xlong,p.xlat,p.height(:,:,i),long,lat); 
@@ -14,6 +16,12 @@ i=m+2;
 h(i)=interps(p.fxlong,p.fxlat,p.fwh,long,lat); 
 u(i)=interps(p.fxlong,p.fxlat,p.uf,long,lat); 
 v(i)=interps(p.fxlong,p.fxlat,p.vf,long,lat); 
+i=m+3;
+% h(i)=interps(p.xlong,p.xlat,p.fwh,long,lat); 
+ua=(p.uah(1:end-1,:)+p.uah(2:end,:))/2;
+va=(p.vah(:,1:end-1)+p.vah(:,2:end))/2;
+u(i)=interps(p.xlong,p.xlat,ua,long,lat); 
+v(i)=interps(p.xlong,p.xlat,va,long,lat); 
 
 names,h,u,v
 end
