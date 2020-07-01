a73483cff9d267e0ed190037d44c02afce068843
Jan Mandel <jan.mandel@gmail.com>
2020-07-01 11:51:02 -0600

    ncdiff compares files with different number of time steps by allowing variables with different dimensions and taking the minimum dimension using in openwfm/WRF-SFIRE#16

--- netcdf/ncdiff.m
+++ netcdf/ncdiff.m
@@ -1,35 +1,50 @@
-function [relerr,ssq,names]=ncdiff(file1,file2,var)
-% [relerr,stdev,p]=ncdiff(file1,file2[,'var'])
+function [relerr,ssq,p,name]=ncdiff(file1,file2,var)
+% [relerr,ssq,p,name]=ncdiff(file1,file2,var)
 % compare variable var in 2 netcdf files
 % if var is missing, compare all variables
+% return structure s with fiels:
 % relerr = max relative difference
 % ssq = square mean relative difference
 % p = p-value, close to +1 -1 means errors significantly in one direction
 % for rounding errors expect relerr=const*eps, p small
+% name = variable name
 if ~exist('var','var'),
     v=ncdump(file1,'-q');
     for i=1:length(v),
         var=v(i).varname;
-        try
-            [relerr(i),ssq(i),names{i}]=ncdiff(file1,file2,var);
-        catch exc
-            disp(exc)
-            relerr(i)=inf;
-        end
+        % try
+            [relerr(i),ssq(i),p(i),name{i}]=ncdiff(file1,file2,var);
+        % catch exc
+         %    disp(exc)
+          %   relerr(i)=inf;
+           %  p(i)=1;
+        % end
     end
     disp('Finished comparing files')
     disp(file1)
     disp(file2)
+    for i=1:length(p),
+       fprintf('%s relative diff %g p-value %g\n',name{i},relerr(i),p(i))
+    end
     return
 end
 v1=ncread(file1,var);
 v2=ncread(file2,var);
 s1=size(v1);
 s2=size(v2);
-if length(s1) ~=  length(s2) | any(s1 ~= s1),
-    error([var,' must be same size 1: ',num2str(s1),' 2: ',num2str(s2)]) 
+if length(s1) ~=  length(s2),
+    error([var,' must be same numer of dimensions 1: ',num2str(s1),' 2: ',num2str(s2)]) 
 end 
-[relerr,ssq]=comp('total',v1,v2);
+if any(s1 ~= s2)
+    warning(['using minimum size of 1:', num2str(s1),' 2: ',num2str(s2)])
+    s=ones(1,4);
+    s(1:length(s1))=min(s1,s2);
+    v1=v1(1:s(1),1:s(2),1:s(3),1:s(4));
+    v2=v2(1:s(1),1:s(2),1:s(3),1:s(4));
+    s1=size(v1);
+    s2=size(v2);
+end
+[relerr,ssq,p]=comp('total',v1,v2);
 if relerr>0,
     for i=1:s1(end)    
         s=['slice ',num2str(i)];
@@ -41,10 +56,10 @@ if relerr>0,
         end
     end
 end
-names=var;
+name=var;
 end
 
-function [relerr,ssq]=comp(s,v1,v2)
+function [relerr,ssq,p]=comp(s,v1,v2)
 scale=max(big(v1),big(v2));
 relerr=big(v2(:)-v1(:))/(scale+realmin); 
 ssq=norm(v2(:)-v1(:))/(max(norm(v1(:)),norm(v2(:)))+realmin); 
