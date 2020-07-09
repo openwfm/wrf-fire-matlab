0885c124910de10926ffd257d8bf0de3d81c8930
James Haley <james.haley@ucdenver.edu>
2020-07-09 08:39:32 -0600

    adding comment, removing typo

--- cycling/insert_analysis.m
+++ cycling/insert_analysis.m
@@ -3,12 +3,12 @@ function outer = insert_analysis(w,paths,a)
 %w = wrfout_tign(f)
 %red = subset_domain(w)
 %paths = graph_dets(w,1)
-%a = squish(paths,1)
+%a = squish(paths,1) , this is an anylysis fire arrival time
 
 red = paths.red;
 w.analysis = w.tign_g;
 temp_a = datenum2time(a,red);
-diff = max(w.tign_g(:))-max(temp_a);p2,
+diff = max(w.tign_g(:))-max(temp_a);
 temp_a = temp_a + diff;
 w.analysis(red.ispan,red.jspan)=temp_a;
 
