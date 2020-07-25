d436d557acac5a982e340cf2d1bcd1249c9276cb
James Haley <james.haley@ucdenver.edu>
2020-07-25 06:04:21 -0600

    updating to out perimeter directory

--- cycling/fire_choice.m
+++ cycling/fire_choice.m
@@ -1,4 +1,4 @@
-function [fire_name,save_name,prefix] = fire_choice()
+function [fire_name,save_name,prefix,perim] = fire_choice()
 
 fire_choice = input_num('which fire? Patch: [0], Camp: [1], Cougar: [3]',0);
 cycle = input_num('Which cycle? ',0,1)
@@ -6,14 +6,17 @@ if fire_choice == 1
     fire_name = 'Camp fire';
     save_name = 'camp';
     prefix='../campTIFs/';
+    perim = '../PERIMs/camp_perims/';
 elseif fire_choice == 0
     fire_name = 'Patch Springs fire';
     save_name = 'patch';
     prefix='../TIFs/';
+    perim = '../PERIMs/patch_perims/';
 else
     fire_name = 'Cougar Creek fire';
     save_name = 'cougar';
     prefix = '../cougarTIFs/';
+    perim = '../PERIMs/cougar_perims/';
 end
 
 end
