3ab42a53833601ae92e75f68fbf0fea1db054b3e
Jan Mandel <jan.mandel@gmail.com>
2020-08-20 10:42:45 -0600

    adding field name arg in spread.m

--- vis3d/spread.m
+++ vis3d/spread.m
@@ -1,9 +1,19 @@
-function spread(p)
-% display fire spread in array of structures p
-% p(i) needs to have fields fire_area and times
+function spread(p,v)
+% spread(p,v)
+% display fire spread in variable v of array of structures p
+% p(i) needs to have fields v and times
+% example:
+%   w=wrfout file name
+%   p=nc2struct(w,{'FIRE_AREA','Times'},{})
+%   spread(p,'fire_area')
+    if ~exist('v','var')
+        v='fire_area';
+    end
     for i=1:length(p),for j=1:size(p(i).fire_area,3)
-        contour(p(i).fire_area(:,:,j))
-        title(['FIRE_AREA ',char(p(i).times(:,j)')],'interpreter','none')
+        contour(p(i).(v)(:,:,j))
+        title([v,' ',char(p(i).times(:,j)')],'interpreter','none')
+        colorbar
         drawnow
+        pause(0.1)
     end
 end
