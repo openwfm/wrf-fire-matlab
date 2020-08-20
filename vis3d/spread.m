bcd71441f0f2d2b86c07ac5eecd87150490f45df
Jan Mandel <jan.mandel@gmail.com>
2020-08-20 10:21:11 -0600

    fmt spread.m

--- vis3d/spread.m
+++ vis3d/spread.m
@@ -1,8 +1,9 @@
 function spread(p)
 % display fire spread in array of structures p
 % p(i) needs to have fields fire_area and times
-for i=1:length(p),for j=1:size(p(i).fire_area,3)
-        pcolor(p(i).fire_area(:,:,j))
+    for i=1:length(p),for j=1:size(p(i).fire_area,3)
+        contour(p(i).fire_area(:,:,j))
         title(['FIRE_AREA ',char(p(i).times(:,j)')],'interpreter','none')
         drawnow
-end,end
+    end
+end
