b4c36ae0e48ab97877b6c5ba87c96bb5a78197dc
Jan Mandel <jan.mandel@gmail.com>
2020-12-11 12:13:58 -0700

    finding scond through inverse iterations

--- femwind/scond.m
+++ femwind/scond.m
@@ -1,5 +1,9 @@
 function condition=scond(K)
-relative_nonsymmetry=big(K-K')/big(K)
-Ks = (K+K')/2;
-e= eigs(Ks,2,'bothendsreal')
-condition= max(e)/min(e);
+R= chol(K);
+afun = @(v) R\(R'\v)
+emax= eigs(K,1,'largestreal')
+emin= 1/eigs(afun,size(K,1),1,'largestreal','IsFunctionSymmetric',true)
+if size(K,1)<5000,
+    e = eig(full(K)); emax_direct=max(e), emin_direct=min(e)
+end
+condition= emax/emin;
