a1a06a17395ca81dd881557d4a29393fdb729102
Jan Mandel <jan.mandel@gmail.com>
2020-12-12 14:31:06 -0700

    test using decomposition in subdomains

--- femwind/rb_schwarz_solve.m
+++ femwind/rb_schwarz_solve.m
@@ -11,28 +11,33 @@ end
 maxit=1000
 tol = 1e-5*big(F)/big(K)
 sub_size=[6,6]
-sub_step=[5,5]
+sub_step=[4,4]
 overlap=sub_size-sub_step
 % index sets
 
-% divide indices
+% divide into intervals in dimensions 1 and 2
 sub_nodes=cell(1,2);
 for i=1:2
-    nsub(i)=ceil(n(i)/sub_step(i));
-    sub_nodes{i}=cell(nsub(i),1); 
-    for j=1:nsub(i)
-        ix = (j-1)*sub_step(i);
-        from = ix+1;
-        to = min(ix+sub_size(i),n(i));
+    msub=ceil(n(i)/sub_step(i));
+    % sub_nodes{i}=cell(msub,1); 
+    for j=1:msub
+        from = (j-1)*sub_step(i)+1;
+        to = min(from+sub_size(i)-1,n(i));
         sub_nodes{i}{j}=[from:to];
         disp(['direction ',num2str(i),' group ',num2str(j),' indices ',num2str(sub_nodes{i}{j})])
-    end
+        if to >= n(i)
+            nsub(i)=j;
+            break
+        end
+     end
 end
 
-% decompose subdomain matrices
-R=cell(nsub);  % cholesky factor
-P=cell(nsub);  % permutation
-ix=cell(nsub); % indices of selection
+check_cover(n(1),sub_nodes{1}(:))
+check_cover(n(2),sub_nodes{2}(:))
+
+
+% global node indices for the subdomains
+IX = cell(nsub);
 for i1=1:nsub(1)
     for i2=1:nsub(2)
         idx1 = sub_nodes{1}{i1}; % horizontal rectangle
@@ -41,29 +46,51 @@ for i1=1:nsub(1)
         disp(['subdomain ',num2str([i1,i2]),' nodes ',num2str(idx1),' by ',num2str(idx2)])
         [ix1,ix2,ix3]=ndgrid(idx1,idx2,idx3);
         ix=sub2ind(n,ix1(:),ix2(:),ix3(:));
-        [R{i1,i2},FLAG,P{i1,i2}]=chol(K(ix,ix));
-        if FLAG,
-            error('Subdomain matrix is not positive definite')
+        IX{i1,i2}=ix;
+    end
+end
+
+% check indices
+check_cover(nn,IX(:))
+
+disp('decomposing subdomain matrices')
+R=cell(nsub);  % cholesky factor
+P=cell(nsub);  % permutation
+for i1=1:nsub(1)
+    for i2=1:nsub(2)
+        idx1 = sub_nodes{1}{i1}; % horizontal rectangle
+        idx2 = sub_nodes{2}{i2};
+        idx3 = 1:n(3); % vertical all
+        ix = IX{i1,i2};
+        [R{i1,i2},ierr,P{i1,i2}]=chol(K(ix,ix),'vector');
+        if ierr
+            error(['Subdomain matrix is not positive definite, error ',num2str(ierr)])
         end
-        fprintf('subdomain %g %g size %g %g %g nodes %g nonzeros %g Cholesky %g\n',...
+        p = P{i1,i2}; r = R{i1,i2}; k = K(ix,ix); err(1) = big(k(p,p)-r'*r);
+        b = rand(length(p),1); x = r\(r'\b(p)); x(p)=x; err(2) = big(k*x-b);
+        fprintf('subdomain %g %g size %g %g %g nodes %g nonzeros %g Cholesky %g error %g %g\n',...
             i1,i2,length(idx1),length(idx2),length(idx3),length(ix),...
-            nnz(K(ix,ix)),nnz(R{i1,i2}))
+            nnz(K(ix,ix)),nnz(R{i1,i2}),err)
     end
 end
         
-        
-
 x = zeros(nn,1);
-colx=1:n(3);
-onex=ones(1,n(3));
+xx = zeros(nn,1);
 for it=1:maxit
     for rb1=1:2
         for rb2=1:2
-            for i1=rb1:2:n(1)
-                for i2=rb2:2:n(2)
+            for i1=rb1:2:nsub(1)
+                for i2=rb2:2:nsub(2)
                     % solving horizontal location i1 i2 and vertical line
-                    ix = sub2ind(n,i1*onex,i2*onex,colx); 
-                    x(ix) = x(ix) - K(ix,ix)\(K(:,ix)'*x - F(ix));
+                    ix = IX{i1,i2};
+                    x(ix) = x(ix) + K(ix,ix)\(F(ix) - K(:,ix)'*x);
+                    p = P{i1,i2}; % Ksub(p,p)= R'*R
+                    res = F(ix) - K(:,ix)'*xx;
+                    sol = R{i1,i2}\(R{i1,i2}'\res(p));
+                    sol(p) = sol;
+                    err = big(res - K(ix,ix)*sol)
+                    xx(ix) = xx(ix) + sol;
+                    err = big(xx(ix)-x(ix))
                 end
             end
         end
@@ -74,3 +101,17 @@ for it=1:maxit
         break
     end
 end
+
+end
+
+function check_cover(m,x)
+% check if cell array x contains indices that cover 1 to m
+c=zeros(m,1);
+for i=1:length(x)
+   c(x{i})=c(x{i})+1;
+end
+if any(c==0),
+    error('subdomains do not cover all nodes')
+end
+end
+
