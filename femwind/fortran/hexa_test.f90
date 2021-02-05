d3a681e3c1480490641637bf1295efb76abe196b
Jan Mandel <jan.mandel@gmail.com>
2021-02-05 11:37:00 -0700

    femwind hexa test builds

---
+++ femwind/fortran/hexa_test.f90
@@ -0,0 +1,23 @@
+program hexa_test
+
+use module_hexa   ! testing only
+
+implicit none
+
+real:: A(3,3), X(3,8), u0(3)    ! fortran is not case sensitive
+integer::iflags(3)
+real:: Kloc(8,8), Floc(8), Jg(3)
+
+call hexa(A,X,u0,Kloc,Floc,Jg,iflags)
+! purpose: create local stiffness matrix etc for hexa 3d element
+! in:
+!   A   coefficient matrix size 3x3, symmetric positive definite
+!   X   nodes coordinates size 3x8, one each column is one node
+!   u0   column vector of input wind at the center of the element
+!   iflags  iflags(1)>0 compute Kloc, iflags(2)>0 compute Floc, iflags(3)>0 compute Jg  
+! out:
+!   Kloc   local stiffness matrix
+!   Floc   local divergence load vector
+!   Jg     gradient at center of function with values V is V'*Jg          
+
+end program hexa_test
--- femwind/fortran/module_hexa.f90
+++ femwind/fortran/module_hexa.f90
@@ -1,10 +1,14 @@
+module module_hexa   ! testing only
+
+contains
+
 subroutine hexa(A,X,u0,Kloc,Floc,Jg,iflags)
 ! purpose: create local stiffness matrix etc for hexa 3d element
 ! in:
 !   A   coefficient matrix size 3x3, symmetric positive definite
 !   X   nodes coordinates size 3x8, one each column is one node
 !   u0   column vector of input wind at the center of the element
-!   iflags  iflag(1)>0 compute Kloc, iflag(2)>0 compute Floc, iflag(3)>0 compute Jg  
+!   iflags  iflags(1)>0 compute Kloc, iflags(2)>0 compute Floc, iflags(3)>0 compute Jg  
 ! out:
 !   Kloc   local stiffness matrix
 !   Floc   local divergence load vector
@@ -13,10 +17,10 @@ subroutine hexa(A,X,u0,Kloc,Floc,Jg,iflags)
 implicit none
 
 !*** arguments
- 
-REAL, INTENT(IN):: A(3,3), X(3,8), u0(3)    ! fortran is not case sensitive
-INTEGER, INTENT(IN)::iflag(3)
-REAL, INTENT(OUT):: Kloc(8,8), Floc(8), Jg(3)
+
+real, intent(in):: A(3,3), X(3,8), u0(3)    ! fortran is not case sensitive
+integer, intent(in)::iflags(3)
+real, intent(out):: Kloc(8,8), Floc(8), Jg(3)
 
 !*** local variables
 
@@ -68,4 +72,6 @@ REAL, INTENT(OUT):: Kloc(8,8), Floc(8), Jg(3)
 !%check_symmetry(Kloc,'Kloc',eps)
 !end
     
-end
+end subroutine hexa
+
+end module module_hexa
