2a2b2db242ccc607012da752536f75a85ea98561
EDShapiro <EDQShapiro@gmail.com>
2021-02-26 15:24:31 -0800

    Updated nd_test.m

--- femwind/nd_test.m
+++ femwind/nd_test.m
@@ -1,17 +1,20 @@
 % test for nd_assembly and nd_mult
 nel=[5,4,3]
 h = [1,1,1]
-expand=1 
+expand=1.3 
 A = diag([1 1 1])
 lambda=[]
 params=[]
 
 X = regular_mesh(nel,h,expand);
 
+
+plot_mesh_3d(X)
+
 K=nd_assembly(A,X,lambda,params);
 
 x=ones(size(X{1}));
-!git 
+
 y=nd_mult(K,x);
 
 err_zero=big(y)  % should be zero
