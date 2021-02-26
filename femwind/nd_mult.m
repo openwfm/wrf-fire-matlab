9cf938f2a388c2df54778b84503927ab80bbe95b
Angela Morrison <angela.morrison@ucdenver.edu>
2021-02-26 15:07:49 -0700

    changes to nd mult for debugging

--- femwind/nd_mult.m
+++ femwind/nd_mult.m
@@ -9,10 +9,11 @@ for i3=1:n3
     for i2=1:n2
         for i1=1:n1
             s=0;
-            for j3=max(i3-1,1):min(i3+1,n3)  % i3-1:i3+1 & avoid overrun
+            for j3=max(i3-1,1):min(i3+1,3)  % i3-1:i3+1 & avoid overrun
                 for j2=max(i2-1,1):min(i2+1,3)
                     for j1=max(i1-1,1):min(i1+1,3)
                         % contribution of K(i,j)*x(j)
+                        print('i1 =', i1, 'i2 =',i2,'i3 =',i3,'j1 =',j1,'j2 =',j2,'j3 =',j3)  
                         s=s+K(i1,i2,i3,2+j1-i1,2+j2-i2,2+j3-i3)*x(j1,j2,j3);
                     end
                 end
