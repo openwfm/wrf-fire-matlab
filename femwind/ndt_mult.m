7fca933c9389df7c0de79ecb0db36aa9f28866e3
Jan Mandel <jan.mandel@gmail.com>
2021-02-28 10:12:27 -0700

    reorg loops in ndt_mult for better readability

--- femwind/ndt_mult.m
+++ femwind/ndt_mult.m
@@ -5,26 +5,27 @@ function y=ndt_mult(K,x)
 t = ndt_storage_table(m); 
 u=reshape(x,n1,n2,n3);  % make x into grid vector if needed
 y=zeros(n1,n2,n3);
-for j3=-1:1                  
-    for j2=-1:1               
-        for j1=-1:1
-            % i offset i=+1,0,-1 to the location of matrix entry (i,j)
-            % in fortran won't have the 2+ because
-            % the array t will be indexed -1:1
-            o1=t(1,2+j1,2+j2,2+j3);  
-            o2=t(2,2+j1,2+j2,2+j3);
-            o3=t(3,2+j1,2+j2,2+j3);
-            % location j of the matrix entry (i,j) in K(io,j) 
-            jx=t(4,2+j1,2+j2,2+j3);
-            for i3=1:n3 
-                k3 = i3 + j3;
-                m3 = i3 + o3;
-                for i2=1:n2
-                    k2 = i2 + j2;
-                    m2 = i2 + o2;
-                    for i1=1:n1
+for i3=1:n3   % global index of row = node i
+    for i2=1:n2
+        for i1=1:n1
+            for j3=-1:1   % relative index j of neighbor = nonzero in the row i               
+                for j2=-1:1               
+                    for j1=-1:1
+                        k3 = i3 + j3; % global index of neighbor node k
+                        k2 = i2 + j2;
                         k1 = i1 + j1;
+                        % relative index of the row where the entry (i,k) 
+                        % is stored, 0 0 0 = this row 
+                        % in fortran we won't have the 2+ because
+                        % the array t will be indexed -1:1
+                        o1=t(1,2+j1,2+j2,2+j3);  
+                        o2=t(2,2+j1,2+j2,2+j3);
+                        o3=t(3,2+j1,2+j2,2+j3);
+                        m3 = i3 + o3;   % row m where the entry (i,k) is stored
+                        m2 = i2 + o2;
                         m1 = i1 + o1;
+                        % index of the matrix entry (i,k) in K(m,:) 
+                        jx=t(4,2+j1,2+j2,2+j3);
                         % contribution of K(i,j)*x(j) if index not out of bounds
                         % in fortran, we won't need to worry about out of bounds
                         % because K and x will be wrapped with zeros
