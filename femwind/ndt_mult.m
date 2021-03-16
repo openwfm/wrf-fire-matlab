d7a8f1e6900f1ea7f5b754e1b95e1b03ad124f49
Jan Mandel <jan.mandel@gmail.com>
2021-03-16 11:25:40 -0600

    simplify ndt_mult and make it closer to fortran

--- femwind/ndt_mult.m
+++ femwind/ndt_mult.m
@@ -11,29 +11,19 @@ for i3=1:n3   % global index of row = node i
             for j3=-1:1   % relative index j of neighbor = nonzero in the row i               
                 for j2=-1:1               
                     for j1=-1:1
-                        % global index triple of neighbor node k 
-                        k3 = i3+j3;
-                        k2 = i2+j2;
-                        k1 = i1+j1;
-                        if ~(k1<1 || k1>n1 || k2<1 || k2>n2 || k3<1 || k3>n3 )                        % relative index of the row where the entry (i,k) 
-                            % row m where the entry (i,k) is stored
+                        % global index triple of neighbor node i+j out of bounds? 
+                        if ~(i1+j1<1 || i1+j1>n1 || i2+j2<1 || i2+j2>n2 || i3+j3<1 || i3+j3>n3 )
+                            % offset m of the m where the entry (i,i+j) is stored, 0 if this row 
                             % in fortran we won't have the 2+ because
                             % the array t will be indexed -1:1
-                            m3 = i3+t(3,2+j1,2+j2,2+j3);
-                            m2 = i2+t(2,2+j1,2+j2,2+j3);
-                            m1 = i1+t(1,2+j1,2+j2,2+j3);
-                            % index of the matrix entry (i,k) in K(m,:) 
-                            jx=     t(4,2+j1,2+j2,2+j3);
+                            m3 = t(3,2+j1,2+j2,2+j3);
+                            m2 = t(2,2+j1,2+j2,2+j3);
+                            m1 = t(1,2+j1,2+j2,2+j3);
+                            % index of the matrix entry (i,i+j) in K(i+m,:) 
+                            jx = t(4,2+j1,2+j2,2+j3);
                             % contribution of K(i,j)*x(j) if index not out of bounds
-                            % in fortran, we won't need to worry about out of bounds
-                            % because K and x will be wrapped with zeros
-                            % if (m1<1 || m1>n1 || m2<1 || m2>n2 || m3<1 || m3>n3 )
-                            %    error('bad m')
-                            % end
-                            % v=K(m1,m2,m3,jx);
-                            % r=u(k1,k2,k3);
-                            y(i1,i2,i3)=y(i1,i2,i3)+K(m1,m2,m3,jx)*u(k1,k2,k3);
-                            %s=y(i1,i2,i3);
+                            y(i1,i2,i3)=y(i1,i2,i3)+K(i1+m1,i2+m2,i3+m3,jx)*u(i1+j1,i2+j2,i3+j3);
+                            % s=y(i1,i2,i3);
                             % d=norm([j1 j2 j3],1);
                             % fprintf('i=(%g %g %g) j=(%g %g %g) d=%g m=(%g %g %g) k=(%g %g %g) v=%g r=%g s=%g\n',...
                             %           [i1 i2 i3     j1 j2 j3    d    m1 m2 m3       k1 k2 k3     v    r   s])
