32fd1d247a975cde0ee14151db03b3f2a508f795
Jan Mandel <jan.mandel@gmail.com>
2021-03-16 17:25:06 -0600

    mv matrix4d_test.f90 ndt_mult_test.f90 #2

--- femwind/fortran/ndt_mult_test.f90
+++ femwind/fortran/ndt_mult_test.f90
@@ -1,8 +1,8 @@
-program matrix4d_test
+program ndt_mult_test
 
-use module_matrix4d   ! testing only
+use module_ndt_mult   ! testing only
 use module_io_matlab ! to read and write matrices as text files from matlab
 
 implicit none
 
-end program matrix4d_test
+end program ndt_mult_test
