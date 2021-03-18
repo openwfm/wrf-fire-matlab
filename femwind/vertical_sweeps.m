92f6a0d4082f247e115b1fe7498b747a6a2a60f4
Angela Morrison <angela.morrison@ucdenver.edu>
2021-03-17 21:29:55 -0600

    Started vertical sweeps function. Not connected yet.

---
+++ femwind/vertical_sweeps.m
@@ -0,0 +1,16 @@
+function [x]=vertical_sweeps(K,F,n,x,params)
+
+
+for rb1=1:2
+    for rb2=1:2
+        for i1=rb1:2:n(1)
+            for i2=rb2:2:n(2)
+            % solving horizontal location i1 i2 and vertical line
+                for i3=1:n(3)
+                    ix = sub2ind(n,i1,i2,i3); 
+                    x(ix) = x(ix) - K(ix,ix)\(K(:,ix)'*x - F(ix));
+                end
+            end
+        end
+    end
+end
