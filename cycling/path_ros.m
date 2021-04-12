92e21611d6765f33c942abd34beb77e2a22946f0
James Haley <james.haley@ucdenver.edu>
2021-04-12 17:02:17 -0600

    add function to compute ros and variance on each path

---
+++ cycling/path_ros.m
@@ -0,0 +1,48 @@
+function [pr,pv] = path_ros(ps)
+
+n = length(ps.paths);
+pr = [];
+pp = [];
+pv = [];
+for i = 2:n
+    p = ps.paths(i).p;
+    for j = 2:length(p)
+%         d = ps.raw_dist(p(j),p(j-1));
+%         t = 24*3600*(ps.points(j,3)-ps.points(j-1,3));
+%         r = d/t;
+%         pr = [pr;r];
+        pp = [pp;p(j-1) p(j)];
+ 
+    end
+end
+pp = unique(pp,'rows');
+n = length(pp);
+for i = 1:n
+
+        d = ps.raw_dist(pp(i,1),pp(i,2));
+        t = (ps.points(pp(i,2),3)-ps.points(pp(i,1),3));
+        r = d/t/(24*3600);
+        pr = [pr;r];
+        rv = ros_var(d,t*24);
+        pv = [pv;rv];
+
+end
+figure,histogram(pr)
+pm = mean(pr);
+ps = std(pr);
+tstr = sprintf('ROS along paths \n Mean: %f  STD: %f',pm,ps);
+title(tstr)
+xlabel('ROS [m/s]')
+ylabel('Frequency')
+
+msk = isnan(pv);
+nan_var = sum(msk(:));
+nan_perc = nan_var/n;
+mean_var = mean(pv(~msk));
+fprintf('Mean of std deviations on paths: %f \n',mean_var)
+fprintf('Percentage of path without variance: %f \n',nan_perc*100);
+figure,histogram(pv(~msk));
+title('Std deviation of ROS on paths')
+
+
+end %function
