c5b45f5eb46781355ac47a30bfc74161e5e5d63d
Jan Mandel <jan.mandel@gmail.com>
2021-04-14 00:29:02 -0600

    apply_boundary_conditions separated for K and F

--- femwind/femwind_main.m
+++ femwind/femwind_main.m
@@ -160,7 +160,8 @@ for sc2 = params.sc2_all
         [K,F,~] = sparse_assembly(A,X,U0,lambda,params);
 
         % dirichlet boundary conditions
-        [K,F]=apply_boundary_conditions(K,F,X);
+        [K,~]=apply_boundary_conditions(K,[],X);   % to K - once
+        [~,F]=apply_boundary_conditions([],F,X);   % to F - every time
 
         % solve the equations
         % [lambda,it] = sparse_solve(K,F,X,'s');
