1faac25e1e2725a6c0fab4469f5c4977cf50c115
Jan Mandel <jan.mandel@gmail.com>
2021-04-16 18:30:22 -0600

    separated coarse_correction

---
+++ femwind/coarse_correction.m
@@ -0,0 +1,24 @@
+function x=coarse_correction(x,F,K,K_coarse,X_coarse,hzc,icl3,X,params)
+    n = size(X{1});
+    F_coarse = restriction(reshape(F-K*x,n),hzc,icl3,X,params);
+    F_coarse = F_coarse(:);
+    if params.apply_coarse_boundary_conditions
+        [K_coarse,F_coarse]=apply_boundary_conditions(K_coarse,F_coarse,X_coarse);
+    end
+    if params.levels<=2 % next is 1, the coarsest
+        x_coarse = K_coarse\F_coarse;
+    else  % solve coarse problem recursively
+        params_coarse=params;  % copy all params 
+        params_coarse.levels=params.levels-1;
+        params_coarse.nsmooth=params.nsmooth_coarse;
+        params_coarse.maxit=params.maxit_coarse;
+        params_coarse.iterations_fig=params.iterations_fig+10;
+        params_coarse.res_slice_fig=params.res_slice_fig+10;
+        params_coarse.err_slice_fig=params.err_slice_fig+10;
+        [x_coarse,~,~,~]=multigrid_solve(K_coarse,F_coarse,X_coarse,params_coarse);
+    end
+    fprintf('coarse solve done, level %g continuting\n',params.levels)
+    % x = x + P*x_coarse
+    x_increment = prolongation(reshape(x_coarse,size(X_coarse{1})),hzc,icl3,X,params);
+    x = x + x_increment(:);
+end
--- femwind/multigrid_solve.m
+++ femwind/multigrid_solve.m
@@ -70,28 +70,7 @@ for it=1:params.maxit
     coarse = mod(it,params.nsmooth+1)==0;
     if coarse
         fprintf('iteration %g level %g coarse correction\n',it,params.levels)
-        % F_coarse = P'*(F - K*x);
-        F_coarse = restriction(reshape(F-K*x,n),hzc,icl3,X,params);
-        F_coarse = F_coarse(:);
-        if params.apply_coarse_boundary_conditions
-            [K_coarse,F_coarse]=apply_boundary_conditions(K_coarse,F_coarse,X_coarse);
-        end
-        if params.levels<=2 % next is 1, the coarsest
-            x_coarse = K_coarse\F_coarse;
-        else  % solve coarse problem recursively
-            params_coarse=params;  % copy all params 
-            params_coarse.levels=params.levels-1;
-            params_coarse.nsmooth=params.nsmooth_coarse;
-            params_coarse.maxit=params.maxit_coarse;
-            params_coarse.iterations_fig=params.iterations_fig+10;
-            params_coarse.res_slice_fig=params.res_slice_fig+10;
-            params_coarse.err_slice_fig=params.err_slice_fig+10;
-            [x_coarse,~,~,~]=multigrid_solve(K_coarse,F_coarse,X_coarse,params_coarse);
-        end
-        fprintf('coarse solve done, level %g continuting\n',params.levels)
-        % x = x + P*x_coarse
-        x_increment = prolongation(reshape(x_coarse,size(X_coarse{1})),hzc,icl3,X,params);
-        x = x + x_increment(:);
+        x=coarse_correction(x,F,K,K_coarse,X_coarse,hzc,icl3,X,params);
         it_type=sprintf('level %g coarse correction',params.levels);
     else
         fprintf('iteration %g level %g smoothing by %s\n',it,params.levels,params.smoothing)
