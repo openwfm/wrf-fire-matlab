51438a37b20856b27ff048b4204b2b2d0ad56276
Jan Mandel <jan.mandel@gmail.com>
2021-04-18 23:48:59 -0600

    coarsening_icl_test.exe compiles

--- femwind/fortran/coarsening_icl_test.f90
+++ femwind/fortran/coarsening_icl_test.f90
@@ -1,82 +1,41 @@
-program prolongation_test
+program coarsening_icl_test
 
 use module_coarsening   
 use module_io_matlab ! to read and write matrices as text files from matlab
 
 implicit none
 
-integer::                                         &
-    ifds, ifde, kfds,kfde, jfds,jfde,             & ! fire grid dimensions
-    ifms, ifme, kfms,kfme, jfms,jfme,             &
-    ifts, ifte, kfts,kfte, jfts,jfte
+real, pointer, dimension(:,:,:):: icl_z_m,dx_m,dy_m,dz_m,A_m,minaspect_m,maxaspect_m ! to read from files
+real, dimension(1,1,1)::cr_x_m,cr_y_m
+real:: A(3,3), dx,dy,minaspect,maxaspect ! to pass on
+real, pointer::dz(:)
+integer, pointer:: icl_z(:)
+integer:: cr_x,cr_y
 
-integer::                             &
-    ifcds, ifcde, kfcds,kfcde, jfcds,jfcde,       & ! coarse grid domain
-    ifcms, ifcme, kfcms,kfcme, jfcms,jfcme,       & ! coarse grid dimensions
-    ifcts, ifcte, kfcts,kfcte, jfcts,jfcte          ! coarse grid tile
+integer::n3
 
-real, pointer, dimension(:,:,:):: u_m, uc_m, X_m, Y_m, Z_m, cl_z_m, hcz_m ! to read from files
-real, pointer, dimension(:,:,:):: u, uc, X, Y, Z  ! to pass on
-integer, pointer:: cl_z(:)  
-integer:: n(3),nc(3),cr_x,cr_y, nwrap = 0
+! read and copy
+call read_array(dx_m,'dx')
+dx=dx_m(1,1,1)
+call read_array(dy_m,'dy')
+dy=dy_m(1,1,1)
+call read_array(dz_m,'dz')
+n3 = size(dz_m)
+allocate(dz(n3))
+dz=reshape(dz_m,(/n3/))
+call read_array(A_m,'A')
+A=reshape(A_m,(/3,3/))
+call read_array(minaspect_m,'minaspect')
+minaspect=minaspect_m(1,1,1)
+call read_array(maxaspect_m,'maxaspect')
+maxaspect=maxaspect_m(1,1,1)
 
-! read matrices, X Y Z already in the ikj ordering
-call read_array(X_m,'X')
-call read_array(Y_m,'Y')
-call read_array(Z_m,'Z')
-call read_array(uc_m,'uc')
-call read_array(cl_z_m,'cl_z')
-call read_array(hcz_m,'hcz')
+call coarsening_icl(cr_x,cr_y,icl_z,dx,dy,dz,A,minaspect,maxaspect)
 
-n = shape(X_m)     ! fine mesh size
-nc = shape(uc_m)   ! coarse mesh size
+cr_x_m(1,1,1)=cr_x
+call write_array(cr_x_m,'cr_x')
+cr_y_m(1,1,1)=cr_y
+call write_array(cr_y_m,'cr_y')
+call write_array(reshape(real(icl_z),(/size(icl_z),1,1/)),'icl_z')
 
-call set_indices(nc,nwrap,                  &    ! coarse mesh bounds
-    ifcds, ifcde, kfcds,kfcde, jfcds,jfcde, & 
-    ifcms, ifcme, kfcms,kfcme, jfcms,jfcme, &
-    ifcts, ifcte, kfcts,kfcte, jfcts,jfcte)
-
-! allocate coarse input and copy
-allocate(uc(ifcms:ifcme,kfcms:kfcme,jfcms:jfcme))
-uc(ifcts:ifcte,kfcts:kfcte,jfcts:jfcte)=uc_m
-
-! set coarsening indices
-if(nc(2).ne.size(cl_z_m))call crash('coarse index array size')
-allocate(cl_z(kfcts:kfcte))
-cl_z = cl_z_m(:,1,1);
-cr_x = hcz_m(1,1,1)
-cr_y = hcz_m(2,1,1)
-
-call set_indices(n,nwrap,                         &     ! coarse mesh bounds
-    ifds, ifde, kfds,kfde, jfds,jfde,             & ! fire grid dimensions
-    ifms, ifme, kfms,kfme, jfms,jfme,             &
-    ifts, ifte, kfts,kfte, jfts,jfte)
-
-! allocate fine input and copy to tile 
-allocate(X(ifms:ifme,kfms:kfme,jfms:jfme))
-X(ifts:ifte,kfts:kfte,jfts:jfte)=X_m
-allocate(Y(ifms:ifme,kfms:kfme,jfms:jfme))
-Y(ifts:ifte,kfts:kfte,jfts:jfte)=Y_m
-allocate(Z(ifms:ifme,kfms:kfme,jfms:jfme))
-Z(ifts:ifte,kfts:kfte,jfts:jfte)=Z_m
-
-! allocate output
-allocate(u(ifms:ifme,kfms:kfme,jfms:jfme))
-
-write(*,*)'calling prolongation'
-call prolongation(   &
-    ifds, ifde, kfds,kfde, jfds, jfde,            & ! fire grid dimensions
-    ifms, ifme, kfms,kfme, jfms, jfme,            &
-    ifts, ifte, kfts, kfte, jfts,jfte,            &
-    ifcds, ifcde, kfcds,kfcde, jfcds,jfcde,       & ! coarse grid domain
-    ifcms, ifcme, kfcms,kfcme, jfcms,jfcme,       & ! coarse grid dimensions
-    ifcts, ifcte, kfcts,kfcte, jfcts,jfcte,       & ! coarse grid tile
-    u,uc,cr_x,cr_y,cl_z,X,Y,Z)
-
-! copy output to tight and write to file
-allocate(u_m(n(1),n(2),n(3)))
-u_m = u(ifts:ifte,kfts:kfte,jfts:jfte)
-call write_array(u_m,'u')
-
-end program prolongation_test
-
+end program coarsening_icl_test
