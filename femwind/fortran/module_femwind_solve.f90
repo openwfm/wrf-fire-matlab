dd365eb81e81f73f9055110a5d53e141fe0f07ea
Jan Mandel <jan.mandel@gmail.com>
2021-04-22 23:55:52 -0600

    adding subroutine femwind_setup

--- femwind/fortran/module_femwind_solve.f90
+++ femwind/fortran/module_femwind_solve.f90
@@ -3,6 +3,28 @@ module module_femwind_solve
        
 contains
 
+subroutine femwind_setup(                         &
+    ifds, ifde, kfds, kfde, jfds, jfde,           & ! fire grid dimensions
+    ifms, ifme, kfms, kfme, jfms, jfme,           &
+    ifps, ifpe, kfps, kfpe, jfps, jfpe,           & ! fire patch bounds
+    ifts, ifte, kfts, kfte, jfts,jfte,            &
+    A, X, Y, Z,                                   & !  inputs
+    K)                                              ! outputs 
+
+integer, intent(in)::                             &
+    ifds, ifde, kfds,kfde, jfds, jfde,            & ! fire grid dimensions
+    ifms, ifme, kfms,kfme, jfms, jfme,            &
+    ifps, ifpe, kfps, kfpe, jfps, jfpe,           & ! fire patch bounds
+    ifts, ifte, kfts, kfte, jfts,jfte            
+
+integer, parameter::msize=14
+real, intent(in), dimension(3,3):: A
+real, intent(in), dimension(ifms:ifme, kfms:kfme, jfms:jfme):: X,Y,Z        !spatial grid at corners
+real,intent(out), dimension(ifms:ifme, kfms:kfme, jfms:jfme, msize):: K     !stiffness matrix
+
+
+end subroutine femwind_setup
+
 subroutine femwind_solve(                         &
     ifds, ifde, kfds, kfde, jfds, jfde,           & ! fire grid dimensions
     ifms, ifme, kfms, kfme, jfms, jfme,           &
@@ -33,6 +55,7 @@ real, dimension(ifms:ifme, kfms:kfme, jfms:jfme):: f
 !*** executable
 
 ! F = div(u0)
+! F = f_assembly_fortran(A,X,U0,lambda,params);
 call f_assembly(                        &
     ifds, ifde, kfds, kfde, jfds, jfde,           & ! fire grid dimensions
     ifms, ifme, kfms, kfme, jfms, jfme,           &
