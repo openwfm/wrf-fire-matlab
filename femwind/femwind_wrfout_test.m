4c8d7c1127a19b546fa43cc2297934770fc8cd41
Angel Farguell Caus <angel.farguell@gmail.com>
2021-04-28 20:34:22 -0600

    practical changes

--- femwind/femwind_wrfout_test.m
+++ femwind/femwind_wrfout_test.m
@@ -1,27 +1,20 @@
 format compact
 % test for ndt_f_assembly from wrfout file
-path='./wrfout_d01_0001-01-01_00:00:00' % path to wrfout file
+path='./wrfout' % path to wrfout file
 [X,u0]=read_fmw_wrfout(path);
 A=diag([1 1 1]);
 lambda=[];
 params=[];
 iflags=[2 1 1];
 iflags=iflags(:);
-addpath ../netcdf
 
-% test same results for ndt_mult from matlab and fortran
-% if exist('fortran/f_assembly_test.exe')
-    disp('testing if same result as fortran')
-    p=params_defaults;
-    p.run_fortran=0;
-    p.run_matlab=1;
-    p.femwind_fortran_test=0
-    err=femwind_fortran(A,X,u0,p);
-    if abs(err)<1e-6
+p=params_defaults;
+p.run_fortran=0;
+p.run_matlab=1;
+p.femwind_fortran_test=0
+[W,err]=femwind_fortran(A,X,u0,p);
+if abs(err)<1e-6
     fprintf('error %g OK\n',err)
-    else
+else
     error(sprintf('error %g too large',err))
-    end
-%else
-%    warning('fortran/f_assembly_test.exe not available')
-%end
+end
--- femwind/read_fmw_wrfout.m
+++ femwind/read_fmw_wrfout.m
@@ -1,12 +1,33 @@
 function [X,u0]=read_fmw_wrfout(path,timestep)
-
+maxsize=200
+addpath ../netcdf
 % if timestep not exist, last timestep
 if ~exist('timestep','var'),
     timestep=size(nc2struct(path,{'Times'},{}).times,2);
 end
 % reading wrfout variables
-p=nc2struct(path,{'U0_FMW','V0_FMW','W0_FMW','HT_FMW','ZSF'},{'DX','DY'},timestep);
+p=nc2struct(path,{'FXLONG','XLONG','U0_FMW','V0_FMW','W0_FMW','HT_FMW','ZSF'},{'DX','DY'},timestep);
+% refinement ratios
+sr_x=size(p.fxlong,1)/size(p.xlong,1);
+sr_y=size(p.fxlong,2)/size(p.xlong,2);
+% fire mesh step
+fdx=p.dx/sr_x;
+fdy=p.dy/sr_y;
 % looking at sizes
+[nx,ny,nz]=size(p.u0_fmw);
+% create sub-case
+if nx>maxsize || ny>maxsize
+    cx=ceil(nx/2);
+    cy=ceil(ny/2);
+    ix=cx-floor(maxsize/2);
+    fx=cx+floor(maxsize/2)-1;
+    iy=cy-floor(maxsize/2);
+    fy=cy+floor(maxsize/2)-1;
+    p.u0_fmw=p.u0_fmw(ix:fx,iy:fy,:);
+    p.v0_fmw=p.v0_fmw(ix:fx,iy:fy,:);
+    p.w0_fmw=p.w0_fmw(ix:fx,iy:fy,:);
+    p.zsf=p.zsf(ix:fx,iy:fy);
+end
 [nx,ny,nz]=size(p.u0_fmw);
 % height profile from the ground
 htt_fmw=zeros(nz+1,1);
@@ -16,7 +37,7 @@ end
 % compute elevation at corners from midpoints
 zsf=midpoints2corners(p.zsf);
 % creating grid 
-[Cx,Cy,Cz]=ndgrid(p.dx*[0:nx],p.dy*[0:ny],htt_fmw);
+[Cx,Cy,Cz]=ndgrid(fdx*[0:nx],fdy*[0:ny],htt_fmw);
 % adding ZSF
 for z=1:nz+1
     Cz(:,:,z)=Cz(:,:,z)+zsf;
