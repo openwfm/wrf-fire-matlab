4ffd98e13465ff005c2bec19d6048b7aea7f5bc2
Jan Mandel <jan.mandel@gmail.com>
2021-04-29 18:53:08 -0600

    splitting module_common off module_femwind

---
+++ femwind/fortran/module_common.f90
@@ -0,0 +1,174 @@
+module module_common 
+
+use module_utils
+
+! declarations of multigrid structure
+integer, parameter::msize=14
+
+
+
+type mg_type
+    real, dimension(3,3):: A                        ! penalty weight matrix
+    real, pointer, dimension(:,:,:):: X, Y, Z       ! grid vertices
+    real, pointer, dimension(:,:,:)::        &      !
+         F, lambda, res                             ! multigrid variables                                       
+    
+    real, pointer, dimension(:,:,:,:):: Kglo        ! global stiffness matrix
+    
+    real:: dx,dy                                    ! horizontal spacing, scalar 
+    real, pointer, dimension(:)::dz                 ! vertical spacing of the layers
+    integer::nx,ny,nz,nn                            ! mesh size in vertices
+  
+    integer::level
+
+    integer:: cr_x, cr_y                            ! coarsening factors
+    integer, pointer, dimension(:):: icl_x, icl_y, icl_z  ! coarsening indices
+
+    integer:: &
+    ifds, ifde, kfds, kfde, jfds, jfde,           & ! fire grid dimensions
+    ifms, ifme, kfms, kfme, jfms, jfme,           & ! memory dimensions
+    ifps, ifpe, kfps, kfpe, jfps, jfpe,           & ! fire patch bounds
+    ifts, ifte, kfts, kfte, jfts, jfte              ! tile dimensions
+ 
+end type
+
+contains
+
+subroutine allocate_mg_level(mg)    !allocate one multigrid level
+type(mg_type),intent(inout)::mg     !multigrid structure
+!*** local
+integer:: &
+    ifds, ifde, kfds, kfde, jfds, jfde,           & ! fire grid dimensions
+    ifms, ifme, kfms, kfme, jfms, jfme,           & ! memory dimensions
+    ifps, ifpe, kfps, kfpe, jfps, jfpe,           & ! fire patch bounds
+    ifts, ifte, kfts, kfte, jfts, jfte              ! tile dimensions
+
+!*** executable
+call get_mg_dims(mg, &
+    ifds, ifde, kfds,kfde, jfds, jfde,            & ! fire grid dimensions
+    ifms, ifme, kfms,kfme, jfms, jfme,            &
+    ifps, ifpe, kfps,kfpe, jfps, jfpe,            & ! fire patch bounds
+    ifts, ifte, kfts,kfte, jfts,jfte)      
+
+if (.not.associated(mg%X))allocate(mg%X(ifms: ifme, kfms: kfme, jfms: jfme))
+if (.not.associated(mg%Y))allocate(mg%Y(ifms: ifme, kfms: kfme, jfms: jfme))
+if (.not.associated(mg%Z))allocate(mg%Z(ifms: ifme, kfms: kfme, jfms: jfme))
+allocate(mg%F(ifms: ifme, kfms: kfme, jfms: jfme))
+allocate(mg%lambda(ifms: ifme, kfms: kfme, jfms: jfme))
+allocate(mg%res(ifms: ifme, kfms: kfme, jfms: jfme))
+
+end subroutine allocate_mg_level
+
+subroutine get_mg_dims(mg_level, &
+    ifds, ifde, kfds, kfde, jfds, jfde,           & ! fire grid dimensions
+    ifms, ifme, kfms, kfme, jfms, jfme,           & ! memory dimensions
+    ifps, ifpe, kfps, kfpe, jfps, jfpe,           & ! fire patch bounds
+    ifts, ifte, kfts, kfte, jfts, jfte)              ! tile dimensions
+implicit none
+type(mg_type),intent(in)::mg_level
+integer, intent(out)::  &
+    ifds, ifde, kfds, kfde, jfds, jfde,           & ! fire grid dimensions
+    ifms, ifme, kfms, kfme, jfms, jfme,           & ! memory dimensions
+    ifps, ifpe, kfps, kfpe, jfps, jfpe,           & ! fire patch bounds
+    ifts, ifte, kfts, kfte, jfts, jfte              ! tile dimensions
+ 
+    ifds = 1
+    ifde = mg_level%nx-1  ! dimension in elements
+    jfds = 1
+    jfde = mg_level%ny-1 
+    kfds = 1
+    kfde = mg_level%nz-1
+
+    ifts = ifds
+    ifte = ifde
+    jfts = jfds
+    jfte = jfde
+    kfts = kfds
+    kfte = kfde
+
+    ifps = ifds
+    ifpe = ifde
+    jfps = jfds
+    jfpe = jfde
+    kfps = kfds
+    kfpe = kfde
+
+    ifms = ifps - 1
+    ifme = ifpe + 2  ! need +1 for vertex grid, and +1 for zero strip 
+    jfms = jfps - 1
+    jfme = jfpe + 2
+    kfms = kfps - 1
+    kfme = kfpe + 2
+
+end subroutine get_mg_dims
+
+subroutine write_3d(mg,l,var,name,v)
+! write array in text file for debugging 
+implicit none
+
+!*** arguments
+type(mg_type),intent(in)::mg(:)    !multigrid structure
+integer,intent(in)::l              !level
+real, intent(in)::var(:,:,:)       !the array
+character(len=*),intent(in)::name  
+character(len=1),intent(in)::v     !'v' or 'e'
+
+!*** local
+integer:: &
+    ifds, ifde, kfds, kfde, jfds, jfde,           & ! fire grid dimensions
+    ifms, ifme, kfms, kfme, jfms, jfme,           & ! memory dimensions
+    ifps, ifpe, kfps, kfpe, jfps, jfpe,           & ! fire patch bounds
+    ifts, ifte, kfts, kfte, jfts, jfte              ! tile dimensions
+integer::ie,ke,je
+character(len=2)::lc
+
+!*** executable
+
+call get_mg_dims(mg(l), &
+    ifds, ifde, kfds,kfde, jfds, jfde,            & ! fire grid dimensions
+    ifms, ifme, kfms,kfme, jfms, jfme,            &
+    ifps, ifpe, kfps,kfpe, jfps, jfpe,           & ! fire patch bounds
+    ifts, ifte, kfts,kfte, jfts,jfte)            
+
+write(lc,'(i2.2)')l
+
+if(v.eq.'v')then
+    ie = snode(ifte,ifde,+1)  ! vertex based
+    je = snode(jfte,jfde,+1)
+    ke = snode(kfte,kfde,+1)
+elseif(v.eq.'e')then
+    ie=ifte
+    je=jfte
+    ke=kfte
+else
+    call crash('write_3d: bad v')
+endif
+ 
+call write_tight(var) ! write dereferenced
+
+contains
+
+subroutine write_tight(varr)
+    real, intent(in)::varr(ifms:ifme,kfms:kfme,jfms:jfme)
+    ! for element/cell/midpoint based arrays, like fmw winds
+    ! open(iu,file='varr.dat',form='unformatted',status='unknown')
+    ! write(iu)varr(ifts:ifte,kfts:kfte,jfts:jfte)
+    ! close(iu)
+    call write_array(varr(ifts:ie,kfts:ke,jfts:je),name//lc)
+end subroutine write_tight
+
+end subroutine write_3d
+
+subroutine print_mg_dims(mg,l)
+type(mg_type),intent(in)::mg(:)  ! multigrid level
+    print *,'level ',mg(l)%level
+    if(associated(mg(l)%X))print *,'X memory shape ',shape(mg(l)%X)
+    if(associated(mg(l)%Y))print *,'Y memory shape ',shape(mg(l)%Y)
+    if(associated(mg(l)%Z))print *,'Z memory shape ',shape(mg(l)%Z)
+    print *,'dx=',mg(l)%dx,' dy=',mg(l)%dy
+    if(associated(mg(l)%dz))print *,'dz=',mg(l)%dz,' shape ',shape(mg(l)%dz)
+    print *,'grid size nx=',mg(l)%nx,' ny=',mg(l)%ny,' nz=',mg(l)%nz, &
+       ' total ndof=',mg(l)%nn
+end subroutine print_mg_dims
+
+end module module_common 
--- femwind/fortran/module_femwind.f90
+++ femwind/fortran/module_femwind.f90
@@ -1,163 +1,26 @@
-! **** TESTING ***
+! **** HOW TO TEST ****
 ! called from femwind_test.f90 compiled as femwind_test.exe
 ! femwind_test.exe is executed from femwind_fortran.m if params.run_fortran
 ! femwind_fortran.m is called from femwind_main.m   
 ! run femwind_main from e.g. femwind_rate_test or femwind_wrfout_test
 
-
-
 module module_femwind
-       use module_utils
-       use module_f_assembly
-       use module_ndt_assembly
-       use module_w_assembly
-       use module_coarsening
-       use module_boundary_conditions
+   use module_utils
+   use module_f_assembly
+   use module_ndt_assembly
+   use module_w_assembly
+   use module_coarsening
+   use module_boundary_conditions
+   use module_common
 
 ! parameters
 integer, parameter::max_levels=20
-integer, parameter::msize=14
 real:: minaspect=1./3.,maxaspect=3.
 real::A(3,3)=reshape((/1., 0., 0.,  0., 1., 0.,  0., 0., 1./),(/3, 3/))
 
 integer::nlevels                                ! number of levels
-
-type mg_type
-    real, dimension(3,3):: A                        ! penalty weight matrix
-    real, pointer, dimension(:,:,:):: X, Y, Z       ! grid vertices
-    real, pointer, dimension(:,:,:,:):: Kglo        ! global stiffness matrix
-    
-    real:: dx,dy                                    ! horizontal spacing, scalar 
-    real, pointer, dimension(:)::dz                 ! vertical spacing of the layers
-    integer::nx,ny,nz,nn                            ! mesh size in vertices
-  
-    integer::level
-
-    integer:: cr_x, cr_y                            ! coarsening factors
-    integer, pointer, dimension(:):: icl_x, icl_y, icl_z  ! coarsening indices
-
-    integer:: &
-    ifds, ifde, kfds, kfde, jfds, jfde,           & ! fire grid dimensions
-    ifms, ifme, kfms, kfme, jfms, jfme,           & ! memory dimensions
-    ifps, ifpe, kfps, kfpe, jfps, jfpe,           & ! fire patch bounds
-    ifts, ifte, kfts, kfte, jfts, jfte              ! tile dimensions
- 
-end type
        
 contains
-
-subroutine get_mg_dims(mg_level, &
-    ifds, ifde, kfds, kfde, jfds, jfde,           & ! fire grid dimensions
-    ifms, ifme, kfms, kfme, jfms, jfme,           & ! memory dimensions
-    ifps, ifpe, kfps, kfpe, jfps, jfpe,           & ! fire patch bounds
-    ifts, ifte, kfts, kfte, jfts, jfte)              ! tile dimensions
-implicit none
-type(mg_type),intent(in)::mg_level
-integer, intent(out)::  &
-    ifds, ifde, kfds, kfde, jfds, jfde,           & ! fire grid dimensions
-    ifms, ifme, kfms, kfme, jfms, jfme,           & ! memory dimensions
-    ifps, ifpe, kfps, kfpe, jfps, jfpe,           & ! fire patch bounds
-    ifts, ifte, kfts, kfte, jfts, jfte              ! tile dimensions
- 
-    ifds = 1
-    ifde = mg_level%nx-1  ! dimension in elements
-    jfds = 1
-    jfde = mg_level%ny-1 
-    kfds = 1
-    kfde = mg_level%nz-1
-
-    ifts = ifds
-    ifte = ifde
-    jfts = jfds
-    jfte = jfde
-    kfts = kfds
-    kfte = kfde
-
-    ifps = ifds
-    ifpe = ifde
-    jfps = jfds
-    jfpe = jfde
-    kfps = kfds
-    kfpe = kfde
-
-    ifms = ifps - 1
-    ifme = ifpe + 2  ! need +1 for vertex grid, and +1 for zero strip 
-    jfms = jfps - 1
-    jfme = jfpe + 2
-    kfms = kfps - 1
-    kfme = kfpe + 2
-
-end subroutine get_mg_dims
-
-subroutine write_3d(mg,l,var,name,v)
-! write array in text file for debugging 
-implicit none
-
-!*** arguments
-type(mg_type),intent(in)::mg(:)    !multigrid structure
-integer,intent(in)::l              !level
-real, intent(in)::var(:,:,:)       !the array
-character(len=*),intent(in)::name  
-character(len=1),intent(in)::v     !'v' or 'e'
-
-!*** local
-integer:: &
-    ifds, ifde, kfds, kfde, jfds, jfde,           & ! fire grid dimensions
-    ifms, ifme, kfms, kfme, jfms, jfme,           & ! memory dimensions
-    ifps, ifpe, kfps, kfpe, jfps, jfpe,           & ! fire patch bounds
-    ifts, ifte, kfts, kfte, jfts, jfte              ! tile dimensions
-integer::ie,ke,je
-character(len=2)::lc
-
-!*** executable
-
-call get_mg_dims(mg(l), &
-    ifds, ifde, kfds,kfde, jfds, jfde,            & ! fire grid dimensions
-    ifms, ifme, kfms,kfme, jfms, jfme,            &
-    ifps, ifpe, kfps,kfpe, jfps, jfpe,           & ! fire patch bounds
-    ifts, ifte, kfts,kfte, jfts,jfte)            
-
-write(lc,'(i2.2)')l
-
-if(v.eq.'v')then
-    ie = snode(ifte,ifde,+1)  ! vertex based
-    je = snode(jfte,jfde,+1)
-    ke = snode(kfte,kfde,+1)
-elseif(v.eq.'e')then
-    ie=ifte
-    je=jfte
-    ke=kfte
-else
-    call crash('write_3d: bad v')
-endif
- 
-call write_tight(var) ! write dereferenced
-
-contains
-
-subroutine write_tight(varr)
-    real, intent(in)::varr(ifms:ifme,kfms:kfme,jfms:jfme)
-    ! for element/cell/midpoint based arrays, like fmw winds
-    ! open(iu,file='varr.dat',form='unformatted',status='unknown')
-    ! write(iu)varr(ifts:ifte,kfts:kfte,jfts:jfte)
-    ! close(iu)
-    call write_array(varr(ifts:ie,kfts:ke,jfts:je),name//lc)
-end subroutine write_tight
-
-end subroutine write_3d
-
-subroutine print_mg_dims(mg,l)
-type(mg_type),intent(in)::mg(:)  ! multigrid level
-    print *,'level ',mg(l)%level
-    if(associated(mg(l)%X))print *,'X memory shape ',shape(mg(l)%X)
-    if(associated(mg(l)%Y))print *,'Y memory shape ',shape(mg(l)%Y)
-    if(associated(mg(l)%Z))print *,'Z memory shape ',shape(mg(l)%Z)
-    print *,'dx=',mg(l)%dx,' dy=',mg(l)%dy
-    if(associated(mg(l)%dz))print *,'dz=',mg(l)%dz,' shape ',shape(mg(l)%dz)
-    print *,'grid size nx=',mg(l)%nx,' ny=',mg(l)%ny,' nz=',mg(l)%nz, &
-       ' total ndof=',mg(l)%nn
-end subroutine print_mg_dims
-
 
 subroutine femwind_setup(mg)
 implicit none
@@ -230,7 +93,7 @@ integer::   &
         ! coarsen dz
 
         allocate(mg(l+1)%dz(mg(l+1)%nz-1))
-	do k=1,mg(l+1)%nz - 1 
+        do k=1,mg(l+1)%nz - 1 
             ! print *,'computing coarse dz of layer ',k
             s = 0.
             do m=mg(l)%icl_z(k),mg(l)%icl_z(k+1)-1
@@ -240,10 +103,13 @@ integer::   &
             mg(l+1)%dz(k)=s
             ! print *,'height dz of coarse layer',k,' is ',s
         enddo
-			
-
     enddo
     print *,nlevels,' levels'
+
+    ! allocate grid variables
+    do l=1,nlevels
+        call allocate_mg_level(mg(l))
+    enddo
 
     ! get coarse grid coordinates 
     do l=1,nlevels-1 
@@ -257,9 +123,6 @@ integer::   &
             ifcms, ifcme, kfcms,kfcme, jfcms, jfcme,            &
             ifcps, ifcpe, kfcps,kfcpe, jfcps, jfcpe,           & ! fire patch bounds
             ifcts, ifcte, kfcts,kfcte, jfcts,jfcte)            
-        allocate(mg(l+1)%X(ifms: ifme, kfms: kfme, jfms: jfme))
-        allocate(mg(l+1)%Y(ifms: ifme, kfms: kfme, jfms: jfme))
-        allocate(mg(l+1)%Z(ifms: ifme, kfms: kfme, jfms: jfme))
         call coarsening_grid(l, &
             ifds, ifde, kfds, kfde, jfds, jfde,           & ! fire grid dimensions
             ifms, ifme, kfms, kfme, jfms, jfme,           & ! memory dimensions
@@ -313,7 +176,7 @@ implicit none
 
 type(mg_type),intent(in)::mg(:)  ! multigrid levels
 
-integer, intent(in)::                             &
+integer, intent(inout)::                             &
     ifds, ifde, kfds,kfde, jfds, jfde,            & ! fire grid dimensions
     ifms, ifme, kfms,kfme, jfms, jfme,            &
     ifps, ifpe, kfps, kfpe, jfps, jfpe,           & ! fire patch bounds
@@ -324,31 +187,26 @@ real,intent(out), dimension(ifms:ifme, kfms:kfme, jfms:jfme)::u, v, w       !mas
 real,intent(out):: rate
 
 !*** local
-real, dimension(ifms:ifme, kfms:kfme, jfms:jfme):: f  
-real, dimension(ifts:ifte+1, kfts:kfte+1, jfts:jfte+1):: f_f ! testing only
 
 !*** executable
 
 ! F = div(u0)
 ! F = f_assembly_fortran(A,X,U0,lambda,params);
+
 call f_assembly(                        &
     ifds, ifde, kfds, kfde, jfds, jfde,           & ! fire grid dimensions
     ifms, ifme, kfms, kfme, jfms, jfme,           &
     ifps, ifpe, kfps, kfpe, jfps, jfpe,           & ! fire patch bounds
-    ifts, ifte, kfts, kfte, jfts,jfte,            &
-    A, mg(1)%X, mg(1)%Y, mg(1)%Z, u0, v0, w0,                       & !Input from femwind, U0, V0, W0 not used in hexa to construct Kloc or JG
-    F)                                             !Global load vector output  
+    ifts, ifte, kfts, kfte, jfts, jfte,           &
+    A, mg(1)%X, mg(1)%Y, mg(1)%Z, u0, v0, w0,     &                    	
+    mg(1)%F)                                        !Global load vector output  
 
 call vec_boundary_conditions(                              &
-    ifds, ifde, kfds,kfde, jfds, jfde,                       & ! fire grid dimensions
-    ifms, ifme, kfms,kfme, jfms, jfme,            &
+    ifds, ifde, kfds, kfde, jfds, jfde,           & ! fire grid dimensions
+    ifms, ifme, kfms, kfme, jfms, jfme,           &
     ifps, ifpe, kfps, kfpe, jfps, jfpe,           & ! fire patch bounds
-    ifts, ifte, kfts, kfte, jfts,jfte,             &
-    F)
-
-
-f_f=f(ifts:ifte+1, kfts:kfte+1, jfts:jfte+1) ! testing only
-call write_array(f_f,'F_f') ! testing only
+    ifts, ifte, kfts, kfte, jfts, jfte,           &
+    mg(1)%F)
 
 ! initialize for now
 u=0.
