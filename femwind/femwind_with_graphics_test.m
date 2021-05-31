8586ed2883c2ccc930d681c80cc6d099bcf2f540
Evan Shapiro <eshapiro@cheyenne4.cheyenne.ucar.edu>
2021-05-31 02:55:56 -0600

    Updated wind_streamlines. To test, run femwind_with_graphics_test.m. !out:

--- femwind/femwind_with_graphics_test.m
+++ femwind/femwind_with_graphics_test.m
@@ -1,12 +1,12 @@
 function p=femwind_with_graphics_test
 disp('femwind_with_graphics_test')
-p=femwind_main_test
+p=femwind_main
 p.graphics=3; 
 p.st_contour=1
 p.sc_all = 1;
 p.sc2_all =  [1];
 p=femwind_main(p);
-rate = [0.066948270621534   0.069839695192657];
+rate = [0.066948270621534];
 if all(abs(p.rate - rate) < 1e-8)
     disp('convergence rate tests OK')
 else
