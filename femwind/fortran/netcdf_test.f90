74cb95a69fb88579da78bb57ef60df403007838f
Jan Mandel <jan.mandel@gmail.com>
2021-09-04 22:51:59 -0600

    netcdf_test reading all steps just to test

--- femwind/fortran/netcdf_test.f90
+++ femwind/fortran/netcdf_test.f90
@@ -11,10 +11,12 @@ integer::sr(2)
 
 !*** executable
 filename = "wrf.nc"
-istep = 1
 
 call ncopen(filename,nf90_nowrite,ncid)
 
+do istep = 1,9999
+
+print *,"step ",istep
 chsum0 = netcdf_read_int_wrf(ncid,"CHSUM0_FMW",istep)
 print *,"CHSUM0=",chsum0
 
@@ -24,7 +26,6 @@ call netcdf_read_array_wrf(ncid,u0_fmw,"U0_FMW",istep,sr)
 call netcdf_read_array_wrf(ncid,v0_fmw,"V0_FMW",istep,sr)
 call netcdf_read_array_wrf(ncid,w0_fmw,"W0_FMW",istep,sr)
 
-call ncclose(ncid)
 
 chsum0_fmw = get_chsum(u0_fmw)
 print *,"chsum0_fmw ", chsum0_fmw
@@ -32,12 +33,19 @@ chsum0_fmw = ieor(chsum0_fmw,get_chsum(v0_fmw))
 print *,"chsum0_fmw ", chsum0_fmw
 chsum0_fmw = ieor(chsum0_fmw,get_chsum(w0_fmw))
 print *,"chsum0_fmw ", chsum0_fmw
-print *,"bound u0_fmw",lbound(u0_fmw),ubound(u0_fmw)
-print *,"bound v0_fmw",lbound(v0_fmw),ubound(v0_fmw)
-print *,"bound w0_fmw",lbound(w0_fmw),ubound(w0_fmw)
-print *,"corner values",u0_fmw(1,1,1),v0_fmw(1,1,1),w0_fmw(1,1,1)
+!print *,"bound u0_fmw",lbound(u0_fmw),ubound(u0_fmw)
+!print *,"bound v0_fmw",lbound(v0_fmw),ubound(v0_fmw)
+!print *,"bound w0_fmw",lbound(w0_fmw),ubound(w0_fmw)
+!print *,"corner values",u0_fmw(1,1,1),v0_fmw(1,1,1),w0_fmw(1,1,1)
 
 print *,"CHSUM0 computed=",chsum0_fmw," difference ",chsum0_fmw-chsum0
+if (chsum0_fmw.ne.chsum0)then
+   print *,'chsum does not agree'
+   stop 1
+endif
 
+enddo
+
+call ncclose(ncid)
 
 end program netcdf_test
