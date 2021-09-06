7f6e6a2d17654d8048022dac7fbdd299116294e4
Jan Mandel <jan.mandel@gmail.com>
2021-09-06 17:32:27 -0600

    femwind_wrfout calling femwind #4

--- femwind/fortran/femwind_wrfout.f90
+++ femwind/fortran/femwind_wrfout.f90
@@ -17,55 +17,55 @@ integer ::                          &
 ! declaratins of A, msize included from module femwind
 
 ! variables read from wrfout
-real, pointer:: u0(:,:,:), v0(:,:,:), w0(:,:,:), zsf(:,:), ht(:)
+real, pointer:: u0_fmw(:,:,:), v0_fmw(:,:,:), w0_fmw(:,:,:), zsf(:,:), ht_fmw(:)
 
-! variables computed here
-real, pointer:: X(:,:,:),Y(:,:,:),Z(:,:,:), &
-                u(:,:,:), v(:,:,:), w(:,:,:),Kmat(:,:,:,:),A_m(:,:,:)
+! variables allocated and computed here
+real, pointer:: u0(:,:,:), v0(:,:,:), w0(:,:,:)
+real, pointer:: u(:,:,:), v(:,:,:), w(:,:,:)
 
-integer:: i, j, k, n(3)
+integer:: i, j, k, n(3), nx, ny, nz
 integer::ncid,frame,sr(2)
-real:: rate,A(3,3),dx,dy
+real:: rate,A(3,3),dx,dy,zx,zy
 character(len=128)::filename 
 
 !*** executable
-filename = "wrf.nc"
+
+filename = "wrf.nc" ! where to read from
 frame = 1
+
 call ncopen(filename,nf90_nowrite,ncid)
 
 ! initial velocity field
-if(read_initial_wind(ncid,u0,v0,w0,frame=frame).ne.0)call crash('check sum does not agree')
+if(read_initial_wind(ncid,u0_fmw,v0_fmw,w0_fmw,frame=frame).ne.0)call crash('check sum does not agree')
 
 ! horizontal height at cell centers
 call netcdf_read_array_wrf(ncid,"ZSF",frame=frame,a2d=zsf) 
 
-! thickness of vertical layers
-call netcdf_read_array_wrf(ncid,"HT_FMW",frame=frame,a1d=ht)
+! height of the vertical layers above the terrain
+call netcdf_read_array_wrf(ncid,"HT_FMW",frame=frame,a1d=ht_fmw)
 
 ! horizontal mesh spacing
-dx = netcdf_read_att(ncid,"DX")
-dy = netcdf_read_att(ncid,"DY")
+call get_sr(ncid,sr)  !  fire subgrid refinement ratio
+dx = netcdf_read_att(ncid,"DX")/sr(1)
+dy = netcdf_read_att(ncid,"DY")/sr(2)
 
-return
+!construct mg data 
 
+params%A = reshape((/1.0,0.0,0.0, &
+                     0.0,1.0,0.0, &
+                     0.0,0.0,1.0 /), &
+                   (/3,3/))
 
-!call read_array(A_m,'A_input')  ! matrices read from Matlab are _m
-!call read_array(X_m,'X_input')
-!call read_array(Y_m,'Y_input')
-!call read_array(Z_m,'Z_input')
+! finest level 1
+mg(1)%nx = size(u0_fmw,1) + 1  ! dimensions are vertex centered
+mg(1)%ny = size(u0_fmw,3) + 1  ! dimensions are vertex centered
+mg(1)%nz = size(u0_fmw,2) + 1  ! dimensions are vertex centered
 
-!params%A = reshape(A_m,(/3,3/))
-
-n = shape(X)
-mg(1)%nx = n(1)
-mg(1)%ny = n(3)
-mg(1)%nz = n(2)
-
-call get_mg_dims(mg(1), &
+call get_mg_dims(mg(1), &                 !  set bounds compatible with WRF
     ifds, ifde, kfds,kfde, jfds, jfde,            & ! fire grid dimensions
     ifms, ifme, kfms,kfme, jfms, jfme,            &
     ifps, ifpe, kfps,kfpe, jfps, jfpe,           & ! fire patch bounds
-    ifts, ifte, kfts,kfte, jfts,jfte)
+    ifts, ifte, kfts,kfte, jfts, jfte)
 
 allocate(mg(1)%X(ifms:ifme,kfms:kfme,jfms:jfme))
 allocate(mg(1)%Y(ifms:ifme,kfms:kfme,jfms:jfme))
@@ -76,26 +76,65 @@ allocate(w0(ifms:ifme,kfms:kfme,jfms:jfme))
 allocate(u(ifms:ifme,kfms:kfme,jfms:jfme)) ! vector components called u v W
 allocate(v(ifms:ifme,kfms:kfme,jfms:jfme))
 allocate(w(ifms:ifme,kfms:kfme,jfms:jfme))
-allocate(Kmat(ifms:ifme,kfms:kfme,jfms:jfme,msize)) ! stifness matrix
 
 ! copy the input data to tile sized bounds
-! X Y Z are corner based, upper bound larger by one
-!do j=jfts,jfte+1
-!  do k=kfts,kfte+1
-!    do i=ifts,ifte+1
-!        mg(1)%X(i,k,j) = X_m(i,k,j)
-!        mg(1)%Y(i,k,j) = Y_m(i,k,j)
-!        mg(1)%Z(i,k,j) = Z_m(i,k,j)
-!    enddo
-!  enddo
-!enddo
+! initial wind is at cell centers, indexing was already switched to ikj in reading
+u0(ifts:ifte,kfts:kfte,jfts:jfte) = u0_fmw
+v0(ifts:ifte,kfts:kfte,jfts:jfte) = v0_fmw
+w0(ifts:ifte,kfts:kfte,jfts:jfte) = w0_fmw
 
-mg(1)%dx = mg(1)%X(2,1,1)-mg(1)%X(1,1,1)
-mg(1)%dy = mg(1)%Y(1,1,2)-mg(1)%Y(1,1,1)
+! X Y Z are corner based, upper bound larger by one
+! first interpolate/extrapolate zsf to corners
+do j=jfts,jfte+1
+  do i=ifts,ifte+1
+    if(i.eq.ifts)then
+      zx = zsf(i,j) - 0.5*(zsf(i+1,j)-zsf(i,j))  ! extrapolate down
+    elseif(i.eq.ifte+1)then
+      zx = zsf(i,j) - 0.5*(zsf(i-1,j)-zsf(i,j))  ! extrapolate up 
+    else
+      zx = 0.5*(zsf(i,j) + zsf(i+1,j))           ! interpolate
+    endif
+    if(j.eq.jfts)then
+      zy = zsf(i,j) - 0.5*(zsf(i,j+1)-zsf(i,j))  ! extrapolate down
+    elseif(j.eq.jfte+1)then
+      zy = zsf(i,j) - 0.5*(zsf(i,j-1)-zsf(i,j))  ! extrapolate up 
+    else
+      zy = 0.5*(zsf(i,j) + zsf(i,j+1))           ! interpolate
+    endif
+    mg(1)%Z(i,kfts,j) = 0.5*(zx + zy)
+  enddo
+enddo
+
+! (X,Y) is the same uniform grid on all levels
+do j=jfts,jfte+1
+  do k=kfts,kfte+1
+    do i=ifts,ifte+1
+        mg(1)%X(i,k,j) = (i-1)*dx
+        mg(1)%Y(i,k,j) = (j-1)*dy
+    enddo
+  enddo
+enddo
+
+! add top of levels above terrain to Z(:,kfts,:)
+print *,'debug ht_fmw=',ht_fmw
+do j=jfts,jfte+1
+  do k=kfts+1,kfte+1
+    do i=ifts,ifte+1
+        mg(1)%Z(i,k,j) = mg(1)%Z(i,kfts,j) + ht_fmw(k - kfts)
+    enddo
+  enddo
+enddo
+
+! mesh spacing
+mg(1)%dx = dx 
+mg(1)%dy = dy
 allocate(mg(1)%dz(mg(1)%nz-1))
 do k=kfds,kfte
     mg(1)%dz(k)=mg(1)%Z(1,k+1,1)-mg(1)%Z(1,k,1)
 enddo
+
+! save memory while solver is running
+deallocate(u0_fmw,v0_fmw,w0_fmw,ht_fmw,zsf)
 
 write(*,'(a)')'calling femwind_setup'
 call femwind_setup(mg)    
@@ -117,5 +156,42 @@ call write_array(u(ifts:ifte,kfts:kfte,jfts:jfte),'u')
 call write_array(v(ifts:ifte,kfts:kfte,jfts:jfte),'v')  
 call write_array(w(ifts:ifte,kfts:kfte,jfts:jfte),'w')  
 call write_scalar(rate,'rate')
+
+! also coordinates for debug/display, even if the caller knows
+call write_average_to_center(mg(1)%X,'X_c')
+call write_average_to_center(mg(1)%Y,'Y_c')
+call write_average_to_center(mg(1)%Z,'Z_c')
+
+contains
+
+subroutine write_average_to_center(F,name)
+! average and return with tight bounds
+implicit none
+!*** arguments
+real, intent(in)::F(ifms:ifme, kfms:kfme, jfms:jfme)
+character(len=*)::name
+!*** local
+real, allocatable::Fm(:,:,:)
+integer::ii,kk,jj
+real::s
+!*** executable
+allocate(Fm(ifts:ifte,kfts:kfte,jfts:jfte))
+do j=jfts,jfte
+  do k=kfts,kfte
+    do i=ifts,ifte
+      s = 0.
+      do ii=0,1
+        do kk=0,1
+          do jj=0,1
+            s = s + F(i+ii,k+kk,j+jj) 
+          enddo
+        enddo
+      enddo
+      Fm(i,k,j) = s/8
+    enddo
+  enddo
+enddo
+call write_array(Fm,name)
+end subroutine write_average_to_center 
 
 end program femwind_wrfout
