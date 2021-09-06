e89a40099f93e9ef5efa09f3b21e721d7ca0cb78
Jan Mandel <jan.mandel@gmail.com>
2021-09-06 11:18:04 -0600

    merging netcdf_test.f90 to femwind_wrfout.f90

--- femwind/fortran/femwind_wrfout.f90
+++ femwind/fortran/femwind_wrfout.f90
@@ -1,14 +1,12 @@
-program femwind_test
+program femwind_wrfout
 
 use module_femwind
 use module_utils
 use module_common
+use module_netcdf
+use module_wrfout
 
 implicit none
-
-real, pointer:: X_m(:,:,:),Y_m(:,:,:),Z_m(:,:,:), &
-                u0_m(:,:,:), v0_m(:,:,:), w0_m(:,:,:),   &
-                u_m(:,:,:), v_m(:,:,:), w_m(:,:,:)
 
 integer ::                          &
     ifds, ifde, kfds, kfde, jfds, jfde,                       & ! fire domain bounds
@@ -22,19 +20,30 @@ real, pointer:: X(:,:,:),Y(:,:,:),Z(:,:,:), &
                 u(:,:,:), v(:,:,:), w(:,:,:),Kmat(:,:,:,:),A_m(:,:,:)
 
 integer:: i, j, k, n(3)
+integer:: ncid,frame=1
 real:: rate
+character(len=128)::filename 
 
-call read_array(A_m,'A_input')  ! matrices read from Matlab are _m
-call read_array(X_m,'X_input')
-call read_array(Y_m,'Y_input')
-call read_array(Z_m,'Z_input')
-call read_array(u0_m, 'u0_input')
-call read_array(v0_m, 'v0_input')
-call read_array(w0_m, 'w0_input')
+!*** executable
+filename = "wrf.nc"
+call ncopen(filename,nf90_nowrite,ncid)
 
-params%A = reshape(A_m,(/3,3/))
+if(read_initial_wind(ncid,u0,v0,w0,frame=frame).ne.0)then
+    print *,'check sum does not agree'
+    stop 1
+endif
 
-n = shape(X_m)
+return
+
+
+!call read_array(A_m,'A_input')  ! matrices read from Matlab are _m
+!call read_array(X_m,'X_input')
+!call read_array(Y_m,'Y_input')
+!call read_array(Z_m,'Z_input')
+
+!params%A = reshape(A_m,(/3,3/))
+
+n = shape(X)
 mg(1)%nx = n(1)
 mg(1)%ny = n(3)
 mg(1)%nz = n(2)
@@ -58,15 +67,15 @@ allocate(Kmat(ifms:ifme,kfms:kfme,jfms:jfme,msize)) ! stifness matrix
 
 ! copy the input data to tile sized bounds
 ! X Y Z are corner based, upper bound larger by one
-do j=jfts,jfte+1
-  do k=kfts,kfte+1
-    do i=ifts,ifte+1
-        mg(1)%X(i,k,j) = X_m(i,k,j)
-        mg(1)%Y(i,k,j) = Y_m(i,k,j)
-        mg(1)%Z(i,k,j) = Z_m(i,k,j)
-    enddo
-  enddo
-enddo
+!do j=jfts,jfte+1
+!  do k=kfts,kfte+1
+!    do i=ifts,ifte+1
+!        mg(1)%X(i,k,j) = X_m(i,k,j)
+!        mg(1)%Y(i,k,j) = Y_m(i,k,j)
+!        mg(1)%Z(i,k,j) = Z_m(i,k,j)
+!    enddo
+!  enddo
+!enddo
 
 mg(1)%dx = mg(1)%X(2,1,1)-mg(1)%X(1,1,1)
 mg(1)%dy = mg(1)%Y(1,1,2)-mg(1)%Y(1,1,1)
@@ -78,18 +87,6 @@ enddo
 write(*,'(a)')'calling femwind_setup'
 call femwind_setup(mg)    
 write(*,'(a)')'femwind_setup returned OK'
-
-! u is midpoint based
-do j=jfts,jfte
-  do k=kfts,kfte
-    do i=ifts,ifte
-        u0(i,k,j) = u0_m(i,k,j)
-        v0(i,k,j) = v0_m(i,k,j)
-        w0(i,k,j) = w0_m(i,k,j)
-    enddo
-  enddo
-enddo
-
 
 write(*,'(a)')'calling femwind_solve'
 call femwind_solve(  mg,&
@@ -108,4 +105,4 @@ call write_array(v(ifts:ifte,kfts:kfte,jfts:jfte),'v')
 call write_array(w(ifts:ifte,kfts:kfte,jfts:jfte),'w')  
 call write_scalar(rate,'rate')
 
-end program femwind_test
+end program femwind_wrfout
