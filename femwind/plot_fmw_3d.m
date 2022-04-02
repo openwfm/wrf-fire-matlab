c14e6b7938e1d2909d2d3e1e92f0f21a96aa4893
Jan Mandel <jan.mandel@gmail.com>
2022-04-01 21:39:11 -0600

    started adding of wind profile

--- femwind/plot_fmw_3d.m
+++ femwind/plot_fmw_3d.m
@@ -6,11 +6,12 @@ xc=zeros(nx,ny,nz);  % cell center coordinates, same as wind
 yc=zeros(nx,ny,nz);
 zc=zeros(nx,ny,nz);
 [xc(:,:,1),yc(:,:,1)]=ndgrid(p.dx*[1:nx],p.dy*[1:ny]);
-zc(:,:,1) = p.zsf + 0.5*p.ht_fmw(1);
+ht=[0;p.ht_fmw]; htc=(ht(2:end)+ht(1:end-1))/2;
+zc(:,:,1) = p.zsf + ht(1);
 for k=2:nz
     xc(:,:,k) = xc(:,:,1);
     yc(:,:,k) = yc(:,:,1);
-    zc(:,:,k) = p.zsf + 0.5*(p.ht_fmw(k-1)+p.ht_fmw(k));
+    zc(:,:,k) = p.zsf + ht(k);
 end
 figure(1)
 plot_wind_3d({xc,yc,zc},{p.u0_fmw,p.v0_fmw,p.w0_fmw},level,scale,stride)
@@ -20,4 +21,10 @@ figure(2)
 plot_wind_3d({xc,yc,zc},{p.u_fmw,p.v_fmw,p.w_fmw},level,scale,stride)
 title('Mass consistent wind')
 xlabel('m');ylabel('m');zlabel('m')
+
+
+    function vprof(i,j)
+        % add vertical wind profile at location i,j
+        ,
+    end
 end
