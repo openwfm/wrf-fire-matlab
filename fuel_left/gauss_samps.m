ee5ae7cf4ef632c1b2af37a03ae322cf3f0aa0bf
James Haley <james.haley@ucdenver.edu>
2022-08-05 14:18:51 -0600

    add another option to make for small fuel time constants

--- fuel_left/gauss_samps.m
+++ fuel_left/gauss_samps.m
@@ -1,16 +1,26 @@
-
-function s = gauss_samps(n,split)
+function s = gauss_samps(n,split,split2)
 
 s = zeros(5,n);
 for i = 1:n
     if rand < split
+        %faster ROS
         s(1:4,i) = 1000*randn(4,1);
     else
+        %slower ros
         s(1:4,i) = 5000*randn(4,1);
     end
 end
 %set fuel time constants
-s(5,:) = 8 + 2000*rand(1,n);
+for i = 1:n
+    if rand < split2
+        %slower burning fuels
+        s(5,i) =  8 + 2000*rand;
+    else
+        %fast burning fuels
+        s(5,i) =  8 + 10*rand;
+    end
+end
+
 
 % figure,histogram(s(1,:))
 % title('t00')
