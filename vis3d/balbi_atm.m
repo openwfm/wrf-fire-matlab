be1242445587d09884c5a77901efb9324a212ed6
Jan Mandel <jan.mandel@gmail.com>
2022-09-12 00:40:04 -0600

    fix comment F -> K

--- vis3d/balbi_atm.m
+++ vis3d/balbi_atm.m
@@ -16,7 +16,7 @@ if nargin<2
 end
 p=nc2struct(f,{'QVAPOR','T','P','PB','T2','PSFC','PH','PHB'},{},s);
 P = p.p+p.pb;            % air pressure
-theta = p.t+300;             % potential temperature in F
+theta = p.t+300;             % potential temperature in K
 % theta = T*(p0/p)^c, p0=1e5, c=0.286 https://en.wikipedia.org/wiki/Potential_temperature    
 temp = theta .* (P*1e-5).^0.286;
 TV=(1+0.61*p.qvapor).*temp; % virtual temperature
@@ -30,4 +30,7 @@ fprintf('average difference P(:,:,1)-PSFC = %g = %g %s\n',dp,100*dp/sp,'%')
 dt=mean(temp(:,:,1)-p.t2,'all');
 mt2=big(p.t2);
 fprintf('average difference temp(:,:1) - T2 = %g max abs t2=%g\n',dt,mt2)
+dz1 = mean(Z(:,:,2)-Z(:,:,1),'all');
+fprintf('average thickness of the lowest layer = %g\n',dz1)
+
 end
