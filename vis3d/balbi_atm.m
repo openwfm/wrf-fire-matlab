57e9802dbe1a7790f5b73832c509d1b5c289e159
Jan Mandel <jan.mandel@gmail.com>
2022-09-12 00:25:29 -0600

    replacing potential temperature by the real thing

--- vis3d/balbi_atm.m
+++ vis3d/balbi_atm.m
@@ -15,9 +15,11 @@ if nargin<2
     s=1;
 end
 p=nc2struct(f,{'QVAPOR','T','P','PB','T2','PSFC','PH','PHB'},{},s);
-T = p.t+300;             % temperature in F
-TV=(1+0.61*p.qvapor).*T; % virtual temperature
 P = p.p+p.pb;            % air pressure
+theta = p.t+300;             % potential temperature in F
+% theta = T*(p0/p)^c, p0=1e5, c=0.286 https://en.wikipedia.org/wiki/Potential_temperature    
+temp = theta .* (P*1e-5).^0.286;
+TV=(1+0.61*p.qvapor).*temp; % virtual temperature
 RHO=P./(287*TV);           % air density
 EL =(p.ph + p.phb)/9.81; % elevation at w-points
 Z = (EL(:,:,1:end-1)+EL(:,:,2:end))/2; % elevation at centers
@@ -25,7 +27,7 @@ Z = (EL(:,:,1:end-1)+EL(:,:,2:end))/2; % elevation at centers
 dp=mean(P(:,:,1)-p.psfc,'all');
 sp=mean(p.psfc,'all');
 fprintf('average difference P(:,:,1)-PSFC = %g = %g %s\n',dp,100*dp/sp,'%')
-dt=mean(T(:,:,1)-p.t2,'all');
+dt=mean(temp(:,:,1)-p.t2,'all');
 mt2=big(p.t2);
-fprintf('average difference T(:,:1)+300 - T2 = %g max abs t2=%g\n',dt,mt2)
+fprintf('average difference temp(:,:1) - T2 = %g max abs t2=%g\n',dt,mt2)
 end
