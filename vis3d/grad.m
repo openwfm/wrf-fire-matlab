7f1cc70bb8d60294c6e993264ef7ae118231312c
Jan <jan.mandel@gmail.com>
2022-10-09 12:16:35 -0600

    tign2ros and tign2rosg (through grad) give same results

--- vis3d/grad.m
+++ vis3d/grad.m
@@ -8,12 +8,12 @@ function [dfdx,dfdy]=grad(x,y,f)
 
 dxx=  x(3:end, 2:end-1)-x(1:end-2, 2:end-1);
 dyx=  y(3:end, 2:end-1)-y(1:end-2, 2:end-1);
-dx=sqrt(dxx.^2+dyx.^2);
-dfdx=(f(3:end, 2:end-1)-f(1:end-2, 2:end-1))./dx;
+dxn=  sqrt(dxx.^2+dyx.^2);
+dfdx=(f(3:end, 2:end-1)-f(1:end-2, 2:end-1))./dxn;
 
 dxy=  x(2:end-1, 3:end)-x(2:end-1, 1:end-2);
 dyy=  y(2:end-1, 3:end)-y(2:end-1, 1:end-2);
-dy=sqrt(dxy.^2+dyy.^2);
-dfdy=(f(2:end-1, 3:end)-f(2:end-1, 1:end-2))./dy;
+dyn=  sqrt(dxy.^2+dyy.^2);
+dfdy=(f(2:end-1, 3:end)-f(2:end-1, 1:end-2))./dyn;
 
-end
+bend
--- vis3d/tign2ros.m
+++ vis3d/tign2ros.m
@@ -1,10 +1,10 @@
 function r=tign2ros(p,i)
 dxlon=(p.fxlong(3:end,2:end-1,i)-p.fxlong(1:end-2,2:end-1,i));
-dxlat=(p.fxlat(3:end,2:end-1,i)-p.fxlong(1:end-2,2:end-1,i));
+dxlat=(p.fxlat(3:end,2:end-1,i)-p.fxlat(1:end-2,2:end-1,i));
 dx=sqrt(dxlon.^2+dxlat.^2);
 gx=(p.tign_g(3:end,2:end-1,i)-p.tign_g(1:end-2,2:end-1,i))./dx;
 dylon=(p.fxlong(2:end-1,3:end,i)-p.fxlong(2:end-1,1:end-2,i));
-dylat=(p.fxlat(2:end-1,3:end,i)-p.fxlong(2:end-1,1:end-2,i));
+dylat=(p.fxlat(2:end-1,3:end,i)-p.fxlat(2:end-1,1:end-2,i));
 dy=sqrt(dylon.^2+dylat.^2);
 gy=(p.tign_g(2:end-1,3:end,i)-p.tign_g(2:end-1,1:end-2,i))./dy;
 gn=sqrt(gx.^2+gy.^2);
