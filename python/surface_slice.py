1c0cf853057c019a15b088eba5b2a10422670502
Jan <jan.mandel@gmail.com>
2023-02-28 18:44:02 -0700

    new file:   surface_slice.py to read pm25 lon lat from wrfout as 2d and write to a file

---
+++ python/surface_slice.py
@@ -0,0 +1,21 @@
+import xarray as xr
+
+file='wrfout.nc'
+new ='smoke.nc'
+
+print('Open the original NetCDF file',file)
+ds = xr.open_dataset(file)
+
+print('Read 2d slices from the last fram')
+pm25 = ds['tr17_1'][-1,0,:,:]
+lon  = ds['XLONG'][-1,:,:]
+lat  = ds['XLAT'][-1,:,:]
+
+print('Create a new dataset with 2d slices')
+new_ds = xr.Dataset({'pm25': pm25, 'lon':lon, 'lat':lat})
+
+print('Create new NetCDF file',new,'and write the dataseet to it')
+new_ds.to_netcdf(new)
+
+
+
