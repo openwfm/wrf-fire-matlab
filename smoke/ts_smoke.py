a348866eae734769b480b8362981ea07a63707db
Jan <jan.mandel@gmail.com>
2023-03-31 14:45:09 -0600

    reading netcdf

--- smoke/ts_smoke.py
+++ smoke/ts_smoke.py
@@ -1,42 +1,29 @@
-import xarray as xr
+import netCDF4 as nc4
 import numpy as np
-import sys, os
+from numpy import concatenate as cat
+import sys, pickle
 
+times = []
 pm25_vars = []
-times_vars = []
 lon_vars = []
 lat_vars = []
 new = sys.argv[-1]
 
+# file_path = "wrfout.nc"
 for file_path in sys.argv[1:-1]:
 
     print('Reading NetCDF file',file_path)
-    ds = xr.open_dataset(file_path)
+    d = nc4.Dataset(file_path,'r')
+    # extract ESMF string times
+    frames = [''.join(x) for x in d.variables['Times'][:].astype(str)]
+    for i in frames:
+       print(i)
+    times     += frames 
+    pm25_vars += [d.variables["tr17_1"][:,0,:,:]]
+    lon_vars  += [d.variables["XLONG"][:,:,:]]
+    lat_vars  += [d.variables["XLAT"][:,:,:]]
 
-    # read lowest layer
-    pm25_vars.append(ds["tr17_1"].isel(bottom_top=0))
+pm25 = cat(pm25_vars,0); del pm25_vars
+lon  = cat(lon_vars,0); del lon_vars
+lat  = cat(lat_vars,0); del lat_vars
 
-    # Get the other variables, Times, longitude, latitude
-    times_vars.append(ds["Times"])
-    lon_vars.append(ds["XLONG"])
-    lat_vars.append(ds["XLAT"])
-
-print('Concatenating the data variables along the time dimension')
-print('pm25')
-pm25 = xr.concat(pm25_vars, dim="Time")
-print('times')
-times = xr.concat(times_vars, dim="Time")
-print('lon')
-lon = xr.concat(lon_vars, dim="Time")
-print('lat')
-lat = xr.concat(lat_vars, dim="Time")
-
-del pm25_vars, times_vars, lon_vars, lat_vars 
-
-ds_new = xr.Dataset({'pm25':pm25,'Times':times,'XLONG':lon,'XLAT':lat})
-
-print('Creating new NetCDF file',new)
-ds_new.to_netcdf(new)
-    
-    
-    
