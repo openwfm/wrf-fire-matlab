8d6d5de59f612e9686274e02c08852e49246fc32
Jan Mandel <jan.mandel@gmail.com>
2023-11-20 13:53:59 -0700

    simplify

--- vis/ts_at_test.m
+++ vis/ts_at_test.m
@@ -16,12 +16,12 @@ function ts_at_test
 
     % Generate xlon and xlat as uniform with random perturbations
     xlon = repmat(linspace(-180, 180, 100), [100, 1]) + rand(100) - 0.5;
-    xlat = repmat(linspace(-90, 90, 100)', [1, 100]) + rand(100, 1) - 0.5;
+    xlat = repmat(linspace(-90, 90, 100)', [1, 100]) + rand(100) - 0.5;
     
     % Define v as a linear function a*xlon + b*xlat
     a = 2;
     b = 3;
-    v = bsxfun(@plus, bsxfun(@times, a, xlon), bsxfun(@times, b, xlat));
+    v=a*xlon + b*xlat;
     v = repmat(v, [1, 1, 10]); % Repeat the same pattern for 10 time steps
     
     % Verify that ts_at returns a*lon + b*lat for several random (lon, lat)
@@ -30,7 +30,8 @@ function ts_at_test
         lat = rand()*180 - 90;  % Random latitude between -90 and 90
         ts = ts_at(lon, lat, xlon, xlat, v);
         expected_ts = a*lon + b*lat;
-        assert(all(abs(ts - expected_ts) < 1e-5), 'Test failed!');
+        err=abs(ts - expected_ts)
+        assert(all(abs(err) < 1e-5), 'Test failed!');
     end
     
     disp('All tests passed.');
