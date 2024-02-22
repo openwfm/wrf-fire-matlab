6e76288db7969ccbe66d7ef40176761c60d75421
Jan Mandel <jan.mandel@gmail.com>
2024-02-22 11:34:42 -0700

    cp core of module_lin_alg.f90 from module_hexa.f90 commit 347b4411b1143651e5 with small mods call of inv3 from hexa_test ok

--- femwind/fortran/module_lin_alg.f90
+++ femwind/fortran/module_lin_alg.f90
@@ -4,7 +4,7 @@ use module_utils
 
 contains
 
-subroutine inv3(M, Inv_M)
+subroutine inv3(M, M_inv)
 !Purpose: Calculate the inverse of a 3X3 matrix
 !In:
 !M  3X3 matrix
@@ -12,35 +12,34 @@ subroutine inv3(M, Inv_M)
 !M_inv Inverse of M 
 implicit none
 real,intent(in), dimension(3,3):: M
-real,intent(out), dimension(3,3):: Inv_M
+real,intent(out), dimension(3,3):: M_inv
 
 !Local Variables
-real, dimension(3,3)::M_T
-real :: det_M, det_M_inv
 
-!!Compute Inverse of M
-det_M =   M(1,1)*(M(2,2)*M(3,3)-M(3,2)*M(2,3)) -  &
-          M(1,2)*(M(1,2)*M(3,3) - M(3,1)*M(2,3))+ &
-          M(1,3)*(M(2,1)*M(2,2) - M(3,1)*M(3,2))
+real :: detM
 
-if(abs(det_M).lt.10*tiny(det_M))then
-        print *,'det_M=',det_M
-        call crash('Inv3: The matrix is numerically singular')
+
+    ! Compute M_inv
+    detM  = (M(1,1)*M(2,2)*M(3,3) - M(1,1)*M(2,3)*M(3,2)-&
+             M(1,2)*M(2,1)*M(3,3) + M(1,2)*M(2,3)*M(3,1)+&
+             M(1,3)*M(2,1)*M(3,2) - M(1,3)*M(2,2)*M(3,1))
+
+    if(abs(detM).lt.10.0*tiny(detM))then
+        print *,'detM=',detM
+        call crash('The matrix is numerically singular')
     endif
 
-det_M_inv = 1./det_M
+    detM = 1.0/detM
 
-M_T   =  transpose(M)
-
-Inv_M(1,1) = det_M_inv*(M_T(2,2)*M_T(3,3) - M_T(2,3)*M_T(3,2))
-Inv_M(1,2) = det_M_inv*(M_T(2,1)*M_T(3,3) - M_T(3,1)*M_T(2,3))
-Inv_M(1,3) = det_M_inv*(M_T(2,1)*M_T(3,2) - M_T(2,2)*M_T(3,1))
-Inv_M(2,1) = det_M_inv*(M_T(1,2)*M_T(3,3) - M_T(3,2)*M_T(1,3))
-Inv_M(2,2) = det_M_inv*(M_T(1,1)*M_T(3,3) - M_T(3,1)*M_T(1,3))
-Inv_M(2,3) = det_M_inv*(M_T(1,1)*M_T(3,2) - M_T(3,1)*M_T(1,2))
-Inv_M(3,1) = det_M_inv*(M_T(1,2)*M_T(2,3) - M_T(1,3)*M_T(2,2))
-Inv_M(3,2) = det_M_inv*(M_T(1,1)*M_T(2,3) - M_T(2,1)*M_T(1,3))
-Inv_M(3,3) = det_M_inv*(M_T(1,1)*M_T(2,2) - M_T(1,2)*M_T(2,1))
+    M_inv(1,1) = +detM * (M(2,2)*M(3,3) - M(2,3)*M(3,2))
+    M_inv(2,1) = -detM * (M(2,1)*M(3,3) - M(2,3)*M(3,1))
+    M_inv(3,1) = +detM * (M(2,1)*M(3,2) - M(2,2)*M(3,1))
+    M_inv(1,2) = -detM * (M(1,2)*M(3,3) - M(1,3)*M(3,2))
+    M_inv(2,2) = +detM * (M(1,1)*M(3,3) - M(1,3)*M(3,1))
+    M_inv(3,2) = -detM * (M(1,1)*M(3,2) - M(1,2)*M(3,1))
+    M_inv(1,3) = +detM * (M(1,2)*M(2,3) - M(1,3)*M(2,2))
+    M_inv(2,3) = -detM * (M(1,1)*M(2,3) - M(1,3)*M(2,1))
+    M_inv(3,3) = +detM * (M(1,1)*M(2,2) - M(1,2)*M(2,1))
 
 end subroutine inv3
 end module module_lin_alg 
