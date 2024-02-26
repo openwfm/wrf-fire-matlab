4df0b2dcb8d37334ebe9d17e449c13c30aa4cc59
Jan Mandel <jan.mandel@gmail.com>
2024-02-25 23:40:52 -0700

    new file:   femwind_run_fortran_rate_test_large.m new file:   femwind_run_fortran_rate_test_medium.m new file:   femwind_run_fortran_rate_test_small.m

---
+++ femwind/femwind_run_fortran_rate_test_small.m
@@ -0,0 +1,11 @@
+function femwind_run_fortran_rate_test
+
+disp('femwind_fortran_rate_test')
+disp('run fortran version only')
+
+p.nelem3=[100,100,10];
+p.run_fortran=1;
+p.run_matlab=0;
+femwind_rate_test(p)
+end
+
